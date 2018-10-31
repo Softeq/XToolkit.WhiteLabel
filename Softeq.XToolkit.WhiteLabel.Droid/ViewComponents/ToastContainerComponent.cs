dd498e0bd011cc057036275019fed0b9596cb8e0
vadim pylsky <vadim.pylsky@softeq.com>
2018-10-31 15:33:22 +0300

    droid: toast service improvements

---
+++ Softeq.XToolkit.WhiteLabel.Droid/ViewComponents/ToastContainerComponent.cs
@@ -0,0 +1,22 @@
+﻿namespace Softeq.XToolkit.WhiteLabel.Droid.ViewComponents
+{
+    public class ToastContainerComponent : IViewComponent<ActivityBase>
+    {
+        public ToastContainerComponent(int containerId)
+        {
+            ContainerId = containerId;
+        }
+
+        public string Key => nameof(ToastContainerComponent);
+
+        public int ContainerId { get; }
+
+        public void Attach(ActivityBase item)
+        {
+        }
+
+        public void Detach(ActivityBase item = null)
+        {
+        }
+    }
+}
