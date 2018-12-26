88badb70f372af27fe6ff0855c31e5d315dc73dc
Eugeny Sampir <2004195@gmail.com>
2018-12-26 10:39:42 +0300

    implemented async commands

---
+++ Softeq.XToolkit.WhiteLabel/Commands/AsyncCommand.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Softeq.XToolkit.WhiteLabel.Commands
+{
+    public abstract class AsyncCommandBase : ICommand
+    {
+        private bool _isRunning;
+
+        public virtual bool CanExecute(object parameter)
+        {
+            return !_isRunning;
+        }
+
+        public async void Execute(object parameter)
+        {
+            if (_isRunning)
+            {
+                return;
+            }
+
+            _isRunning = true;
+
+            try
+            {
+                await ExecuteAsync(parameter).Invoke().ConfigureAwait(false);
+            }
+            finally
+            {
+                Threading.Execute.OnUIThread(() =>
+                {
+                    _isRunning = false;
+                    CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+                });
+            }
+        }
+
+        protected abstract Func<Task> ExecuteAsync(object parameter);
+
+        public event EventHandler CanExecuteChanged;
+    }
+
+    public class AsyncCommand : AsyncCommandBase
+    {
+        private readonly Func<Task> _action;
+
+        public AsyncCommand()
+        {
+        }
+
+        public AsyncCommand(Func<Task> myAsyncFunction)
+        {
+            _action = myAsyncFunction;
+        }
+
+        protected override Func<Task> ExecuteAsync(object parameter)
+        {
+            return _action;
+        }
+    }
+
+    public class AsyncCommand<T> : AsyncCommandBase
+    {
+        private readonly Func<T, Task> _action;
+
+        public AsyncCommand(Func<T, Task> myAsyncFunction)
+        {
+            _action = myAsyncFunction;
+        }
+
+        protected override Func<Task> ExecuteAsync(object parameter)
+        {
+            return () => _action((T) parameter);
+        }
+    }
+}
