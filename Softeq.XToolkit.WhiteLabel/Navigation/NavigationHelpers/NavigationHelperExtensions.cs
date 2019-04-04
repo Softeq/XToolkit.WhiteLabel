8209ffc4875b9c87e112a0de9fb1e3971c1083d1
s.leushunou <s.leushunou@pixelplex.io>
2019-04-04 15:30:24 +0300

    remove PH1 headers

--- Softeq.XToolkit.WhiteLabel/Navigation/NavigationHelpers/NavigationHelperExtensions.cs
+++ Softeq.XToolkit.WhiteLabel/Navigation/NavigationHelpers/NavigationHelperExtensions.cs
@@ -1,5 +1,5 @@
-// Developed for PAWS-HALO by Softeq Development
-// Corporation http://www.softeq.com
+// Developed by Softeq Development Corporation
+// http://www.softeq.com
 
 using System.Collections.Generic;
 using Softeq.XToolkit.Common.Extensions;
