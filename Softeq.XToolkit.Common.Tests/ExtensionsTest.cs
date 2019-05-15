bb024521591e57b5dda734de3fbd56244b03bd8d
Siarhei Leushunou <siarhei.leushunou@softeq.com>
2019-05-15 17:06:47 +0300

    add common tests

--- Softeq.XToolkit.Common.Tests/ExtensionsTest.cs
+++ Softeq.XToolkit.Common.Tests/ExtensionsTest.cs
@@ -11,7 +11,7 @@ namespace Softeq.XToolkit.Tests.Core.Common
         public void GetAssemblyNameTest()
         {
             var name = GetType().Assembly.GetName().Name;
-            Assert.Equal(name, "Softeq.XToolkit.Tests.Core");
+            Assert.Equal(name, "Softeq.XToolkit.Common.Tests");
         }
 
         [Fact]
