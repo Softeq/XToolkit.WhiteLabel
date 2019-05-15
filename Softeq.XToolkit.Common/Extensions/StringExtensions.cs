25f68ea72332a7f5ad5aaa1f0a898e9117eee259
Siarhei Leushunou <siarhei.leushunou@softeq.com>
2019-05-15 18:25:52 +0300

    add helpers

---
+++ Softeq.XToolkit.Common/Extensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Softeq.XToolkit.Common.Extensions
+{
+    public static class StringExtensions
+    {
+        private const string LinkPattern = @"[a-z]+://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?";
+
+        public static IEnumerable<Capture> FindLinks(this string self)
+        {
+            foreach (Capture match in Regex.Matches(self, LinkPattern, RegexOptions.IgnoreCase))
+            {
+                yield return match;
+            }
+        }
+    }
+}
