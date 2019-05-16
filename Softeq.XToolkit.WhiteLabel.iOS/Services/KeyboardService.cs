f96342ec8551c69eb253513791a1c2592984b7ba
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2019-05-16 16:02:01 +0300

    Add keyboard service

---
+++ Softeq.XToolkit.WhiteLabel.iOS/Services/KeyboardService.cs
@@ -0,0 +1,15 @@
+﻿using UIKit;
+
+namespace Softeq.XToolkit.WhiteLabel.iOS.Services
+{
+    public static class KeyboardService
+    {
+        /// <summary>
+        /// Hide the keyboard globally.
+        /// </summary>
+        public static void HideKeyboard()
+        {
+            UIApplication.SharedApplication.SendAction(new ObjCRuntime.Selector("resignFirstResponder"), null, null, null);
+        }
+    }
+}
