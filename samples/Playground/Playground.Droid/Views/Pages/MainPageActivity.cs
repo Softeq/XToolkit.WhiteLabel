c1a641eeac94513d7d643cc779ab8703512bcdf4
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2019-05-26 20:50:32 +0300

    Add Droid integration

---
+++ samples/Playground/Playground.Droid/Views/Pages/MainPageActivity.cs
@@ -0,0 +1,21 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+using Android.App;
+using Android.OS;
+using Softeq.XToolkit.WhiteLabel.Droid;
+using Playground.ViewModels.Pages;
+
+namespace Playground.Droid.Views.Pages
+{
+    [Activity(Theme = "@style/AppTheme")]
+    public class MainPageActivity : ActivityBase<MainPageViewModel>
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+        }
+    }
+}
