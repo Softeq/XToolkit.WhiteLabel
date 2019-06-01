9fb167a8f181e35a8839b3ceeed808fc1ca4eacd
Pavel Leonenko <pavel.leonenko@softeq.com>
2019-06-02 00:08:47 +0300

    Redundant/not working methods removed, location permission tweaked

--- Softeq.XToolkit.WhiteLabel/Mvvm/RootFrameNavigationViewModelBase.cs
+++ Softeq.XToolkit.WhiteLabel/Mvvm/RootFrameNavigationViewModelBase.cs
@@ -7,28 +7,14 @@ namespace Softeq.XToolkit.WhiteLabel.Mvvm
 {
     public abstract class RootFrameNavigationViewModelBase : ViewModelBase
     {
-        private bool _canGoBack;
-
         protected RootFrameNavigationViewModelBase(IFrameNavigationService frameNavigationService)
         {
             FrameNavigationService = frameNavigationService;
         }
 
-        public bool CanGoBack
-        {
-            get => _canGoBack;
-            set => Set(ref _canGoBack, value);
-        }
-
         public new bool IsInitialized => FrameNavigationService.IsInitialized;
 
         public abstract void NavigateToFirstPage();
-
-        public override void OnInitialize()
-        {
-            base.OnInitialize();
-            CanGoBack = FrameNavigationService.CanGoBack;
-        }
 
         public void InitializeNavigation(object navigation)
         {
