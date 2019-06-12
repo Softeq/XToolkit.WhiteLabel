d16a37e3b044b5b8a4c0d91ec9398128c8b330a6
Sergey Levshunov <sergeylevshunov@Sergeys-MacBook-Pro.local>
2019-06-12 14:09:42 +0300

    add documentation for some classes

--- Softeq.XToolkit.Common/Collections/INotifyGroupCollectionChanged.cs
+++ Softeq.XToolkit.Common/Collections/INotifyGroupCollectionChanged.cs
@@ -7,8 +7,14 @@ using Softeq.XToolkit.Common.EventArguments;
 
 namespace Softeq.XToolkit.Common.Collections
 {
+    /// <summary>
+    ///     Notifies listeners of dynamic changes, such as when a list of items is added and removed or the whole list is cleared.
+    /// </summary>
     public interface INotifyGroupCollectionChanged : INotifyCollectionChanged
     {
+        /// <summary>
+        ///     Occurs when the collection changes.
+        /// </summary>
         event EventHandler<NotifyKeyGroupsCollectionChangedEventArgs> ItemsChanged;
     }
 }
