716d8b5bda806605abaa4a3991ed26a64d385e40
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2019-06-12 20:18:49 +0300

    Add headers

--- Softeq.XToolkit.WhiteLabel.Droid/Controls/BadgeView.cs
+++ Softeq.XToolkit.WhiteLabel.Droid/Controls/BadgeView.cs
@@ -1,4 +1,6 @@
-﻿
+// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- samples/Playground/Playground.iOS/CustomIosBootstraper.cs
+++ samples/Playground/Playground.iOS/CustomIosBootstraper.cs
@@ -1,3 +1,6 @@
+// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
 ﻿using Autofac;
 using Softeq.XToolkit.WhiteLabel.iOS;
 
