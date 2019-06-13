5b1c970beffcfded1924452af3b5d64bcc2ed5c0
Sergey Levshunov <sergeylevshunov@Sergeys-MacBook-Pro.local>
2019-06-13 15:06:00 +0300

    add weakreference documentation

--- Softeq.XToolkit.Common/WeakReferenceEx.cs
+++ Softeq.XToolkit.Common/WeakReferenceEx.cs
@@ -7,21 +7,39 @@ namespace Softeq.XToolkit.Common
 {
     public static class WeakReferenceEx
     {
+        /// <summary>
+        /// Initializes a new instance of the WeakReferenceEx class, referencing the specified object.
+        /// </summary>
+        /// <returns>New WeakReferenceEx object.</returns>
+        /// <param name="target">The object to track.</param>
+        /// <typeparam name="T">Target type.</typeparam>
         public static WeakReferenceEx<T> Create<T>(T target) where T : class
         {
             return new WeakReferenceEx<T>(target);
         }
     }
 
+    /// <summary>
+    /// Represents a weak reference, which references an object while still allowing that object to be reclaimed by garbage collection.
+    /// </summary>
+    /// <typeparam name="T">Target type.</typeparam>
     public class WeakReferenceEx<T> where T : class
     {
         private readonly WeakReference<T> _weakReference;
 
+        /// <summary>
+        /// Initializes a new instance of the WeakReferenceEx class, referencing the specified object.
+        /// </summary>
+        /// <param name="obj">The object to track.</param>
         public WeakReferenceEx(T obj)
         {
             _weakReference = new WeakReference<T>(obj);
         }
 
+        /// <summary>
+        /// Gets the target.
+        /// </summary>
+        /// <value>Target object if the target was retrieved; otherwise, null.</value>
         public T Target
         {
             get
