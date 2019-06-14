202be7bc65ee18011706c70cbb07e01a340a7898
Yauhen Sampir <ysampir@gmail.com>
2019-06-15 02:17:52 +0300

    removed unused code

--- samples/Playground/Playground.iOS/ViewControllers/Pages/Temp/RedViewController.cs
+++ samples/Playground/Playground.iOS/ViewControllers/Pages/Temp/RedViewController.cs
@@ -18,8 +18,6 @@ namespace Playground.iOS.ViewControllers.Pages.Temp
         {
             base.ViewDidLoad();
             Button.SetCommand(ViewModel.NavigateCommand);
-
-            Console.WriteLine("App Started");
         }
     }
 }
