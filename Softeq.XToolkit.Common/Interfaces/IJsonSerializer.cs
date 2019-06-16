d79b6f33c0e6df28f6bf129cb3011fd0bba11284
Sergey Levshunov <sergeylevshunov@Sergeys-MacBook-Pro.local>
2019-06-16 17:28:47 +0300

    add doc for some interfaces

--- Softeq.XToolkit.Common/Interfaces/IJsonSerializer.cs
+++ Softeq.XToolkit.Common/Interfaces/IJsonSerializer.cs
@@ -6,11 +6,40 @@ using System.Threading.Tasks;
 
 namespace Softeq.XToolkit.Common.Interfaces
 {
+    /// <summary>
+    /// Represents methods for serialization and deserialization objects into and from the JSON format
+    /// </summary>
     public interface IJsonSerializer
     {
+        /// <summary>
+        /// Serializes the specified object to a JSON string.
+        /// </summary>
+        /// <param name="value">The object to serialize.</param>
+        /// <returns>A JSON string representation of the object.</returns>
         string Serialize(object value);
+
+        /// <summary>
+        /// Deserializes the JSON to a .NET object.
+        /// </summary>
+        /// <param name="value">The Stream that contains the JSON structure to deserialize.</param>
+        /// <returns>The deserialized object from the JSON string.</returns>
+        /// <typeparam name="T">Object type.</typeparam>
         T Deserialize<T>(string value);
+
+        /// <summary>
+        /// Deserializes the JSON to a .NET object.
+        /// </summary>
+        /// <param name="stream">Stream The JSON to deserialize.</param>
+        /// <returns>The deserialized object from the JSON string.</returns>
+        /// <typeparam name="T">Object type.</typeparam>
         Task<T> DeserializeAsync<T>(Stream stream);
+
+        /// <summary>
+        /// Asynchronously Serializes the specified object to a JSON string.
+        /// </summary>
+        /// <param name="obj">The object to serialize.</param>
+        /// <param name="stream">The Stream used to write the JSON structure.</param>
+        /// <returns>A JSON string representation of the object.</returns>
         Task SerializeAsync(object obj, Stream stream);
     }
 }
--- Softeq.XToolkit.Common/Interfaces/ITimer.cs
+++ Softeq.XToolkit.Common/Interfaces/ITimer.cs
@@ -3,10 +3,25 @@
 
 namespace Softeq.XToolkit.Common.Interfaces
 {
+    /// <summary>
+    /// Represents methods and properties to execute an action in a time interval.
+    /// </summary>
 	public interface ITimer
-	{
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether the timer should run action.
+        /// </summary>
+        /// <value><c>true</c> if Task should run; otherwise, <c>false</c>.</value>
 		bool IsActive { get; }
-		void Start();
-		void Stop();
+
+        /// <summary>
+        /// Starts runnung task by setting Enabled to true.
+        /// </summary>
+        void Start();
+
+        /// <summary>
+        /// Stops runnung task by setting Enabled to false.
+        /// </summary>
+        void Stop();
 	}
 }
