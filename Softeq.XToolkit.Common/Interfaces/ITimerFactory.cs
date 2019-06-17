77f4e817d114afc91aa15a2c422cf9992dff9046
Sergey Levshunov <sergeylevshunov@Sergeys-MacBook-Pro.local>
2019-06-17 14:59:27 +0300

    add log and timerfactory doc

--- Softeq.XToolkit.Common/Interfaces/ITimerFactory.cs
+++ Softeq.XToolkit.Common/Interfaces/ITimerFactory.cs
@@ -4,7 +4,13 @@
 namespace Softeq.XToolkit.Common.Interfaces
 {
 	public interface ITimerFactory
-	{
+    {
+        /// <summary>
+        /// Initializes a new instance of the timer with specified interval.
+        /// </summary>
+        /// <param name="taskReference">Task to be executed at specified interval.</param>
+        /// <param name="interval">Timer interval (ms).</param>
+        /// <returns>Returns timer object.</returns>
 		ITimer Create(TaskReference taskReference, int interval);
 	}
 }
