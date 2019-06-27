6d1c3dc4a0a0c16fd699881a348d8084c6017773
Timofey Burak <timofey.burak@softeq.com>
2019-06-27 13:08:48 +0300

    Implement base WeakDelegate class

--- Softeq.XToolkit.Common/WeakAction.cs
+++ Softeq.XToolkit.Common/WeakAction.cs
@@ -3,21 +3,11 @@
 
 using System;
 using System.Diagnostics.CodeAnalysis;
-using System.Reflection;
 
 namespace Softeq.XToolkit.Common
 {
-    public class WeakAction
+    public class WeakAction : WeakDelegate<Action>
     {
-        private Action _staticAction;
-
-        /// <summary>
-        ///     Initializes an empty instance of the <see cref="WeakAction" /> class.
-        /// </summary>
-        protected WeakAction()
-        {
-        }
-
         /// <summary>
         ///     Initializes a new instance of the <see cref="WeakAction" /> class.
         /// </summary>
@@ -37,97 +27,9 @@ namespace Softeq.XToolkit.Common
             "CA1062:Validate arguments of public methods",
             MessageId = "1",
             Justification = "Method should fail with an exception if action is null.")]
-        public WeakAction(object target, Action action)
+        public WeakAction(object target, Action action) : base(target, action)
         {
-            if (action.GetMethodInfo().IsStatic)
-            {
-                _staticAction = action;
-
-                if (target != null)
-                {
-                    // Keep a reference to the target to control the
-                    // WeakAction's lifetime.
-                    Reference = new WeakReference(target);
-                }
-
-                return;
-            }
-
-            Method = action.GetMethodInfo();
-            ActionReference = new WeakReference(action.Target);
-            Reference = new WeakReference(target);
         }
-
-        /// <summary>
-        ///     Gets or sets the <see cref="MethodInfo" /> corresponding to this WeakAction's
-        ///     method passed in the constructor.
-        /// </summary>
-        protected MethodInfo Method { get; set; }
-
-        /// <summary>
-        ///     Gets the name of the method that this WeakAction represents.
-        /// </summary>
-        public virtual string MethodName => _staticAction != null ? _staticAction.GetMethodInfo().Name : Method.Name;
-
-        /// <summary>
-        ///     Gets or sets a WeakReference to this WeakAction's action's target.
-        ///     This is not necessarily the same as
-        ///     <see cref="Reference" />, for example if the
-        ///     method is anonymous.
-        /// </summary>
-        protected WeakReference ActionReference { get; set; }
-
-        /// <summary>
-        ///     Gets or sets a WeakReference to the target passed when constructing
-        ///     the WeakAction. This is not necessarily the same as
-        ///     <see cref="ActionReference" />, for example if the
-        ///     method is anonymous.
-        /// </summary>
-        protected WeakReference Reference { get; set; }
-
-        /// <summary>
-        ///     Gets a value indicating whether the WeakAction is static or not.
-        /// </summary>
-        public bool IsStatic => _staticAction != null;
-
-        /// <summary>
-        ///     Gets a value indicating whether the Action's owner is still alive, or if it was collected
-        ///     by the Garbage Collector already.
-        /// </summary>
-        public virtual bool IsAlive
-        {
-            get
-            {
-                if (_staticAction == null
-                    && Reference == null)
-                {
-                    return false;
-                }
-
-                if (_staticAction != null)
-                {
-                    if (Reference != null)
-                    {
-                        return Reference.IsAlive;
-                    }
-
-                    return true;
-                }
-
-                return Reference.IsAlive;
-            }
-        }
-
-        /// <summary>
-        ///     Gets the Action's owner. This object is stored as a
-        ///     <see cref="WeakReference" />.
-        /// </summary>
-        public object Target => Reference?.Target;
-
-        /// <summary>
-        ///     The target of the weak reference.
-        /// </summary>
-        protected object ActionTarget => ActionReference?.Target;
 
         /// <summary>
         ///     Executes the action. This only happens if the action's owner
@@ -135,38 +37,13 @@ namespace Softeq.XToolkit.Common
         /// </summary>
         public void Execute()
         {
-            if (_staticAction != null)
+            if (StaticDelegate != null)
             {
-                _staticAction();
+                StaticDelegate.Invoke();
                 return;
             }
 
-            var actionTarget = ActionTarget;
-
-            if (IsAlive)
-            {
-                if (Method != null
-                    && ActionReference != null
-                    && actionTarget != null)
-                {
-                    Method.Invoke(actionTarget, null);
-
-                    // ReSharper disable RedundantJumpStatement
-                    return;
-                    // ReSharper restore RedundantJumpStatement
-                }
-            }
-        }
-
-        /// <summary>
-        ///     Sets the reference that this instance stores to null.
-        /// </summary>
-        public void MarkForDeletion()
-        {
-            Reference = null;
-            ActionReference = null;
-            Method = null;
-            _staticAction = null;
+            TryExecuteWeakDelegate<object>();
         }
     }
 
@@ -176,10 +53,8 @@ namespace Softeq.XToolkit.Common
     /// </summary>
     /// <typeparam name="T">The type of the Action's parameter.</typeparam>
     ////[ClassInfo(typeof(WeakAction))]
-    public class WeakAction<T> : WeakAction, IExecuteWithObject
+    public class WeakAction<T> : WeakDelegate<Action<T>>, IExecuteWithObject
     {
-        private Action<T> _staticAction;
-
         /// <summary>
         ///     Initializes a new instance of the WeakAction class.
         /// </summary>
@@ -199,54 +74,7 @@ namespace Softeq.XToolkit.Common
             "CA1062:Validate arguments of public methods",
             MessageId = "1",
             Justification = "Method should fail with an exception if action is null.")]
-        public WeakAction(object target, Action<T> action)
-        {
-            if (action.GetMethodInfo().IsStatic)
-            {
-                _staticAction = action;
-
-                if (target != null)
-                {
-                    // Keep a reference to the target to control the
-                    // WeakAction's lifetime.
-                    Reference = new WeakReference(target);
-                }
-
-                return;
-            }
-
-            Method = action.GetMethodInfo();
-            ActionReference = new WeakReference(action.Target);
-            Reference = new WeakReference(target);
-        }
-
-        /// <summary>
-        ///     Gets the name of the method that this WeakAction represents.
-        /// </summary>
-        public override string MethodName => _staticAction != null ? _staticAction.GetMethodInfo().Name : Method.Name;
-
-        /// <summary>
-        ///     Gets a value indicating whether the Action's owner is still alive, or if it was collected
-        ///     by the Garbage Collector already.
-        /// </summary>
-        public override bool IsAlive
-        {
-            get
-            {
-                if (_staticAction == null
-                    && Reference == null)
-                {
-                    return false;
-                }
-
-                if (_staticAction != null)
-                {
-                    return Reference == null || Reference.IsAlive;
-                }
-
-                return Reference.IsAlive;
-            }
-        }
+        public WeakAction(object target, Action<T> action) : base(target, action) { }
 
         /// <summary>
         ///     Executes the action with a parameter of type object. This parameter
@@ -265,54 +93,19 @@ namespace Softeq.XToolkit.Common
         }
 
         /// <summary>
-        ///     Sets all the actions that this WeakAction contains to null,
-        ///     which is a signal for containing objects that this WeakAction
-        ///     should be deleted.
-        /// </summary>
-        public new void MarkForDeletion()
-        {
-            _staticAction = null;
-            base.MarkForDeletion();
-        }
-
-        /// <summary>
-        ///     Executes the action. This only happens if the action's owner
-        ///     is still alive. The action's parameter is set to default(T).
-        /// </summary>
-        public new void Execute()
-        {
-            Execute(default(T));
-        }
-
-        /// <summary>
         ///     Executes the action. This only happens if the action's owner
         ///     is still alive.
         /// </summary>
         /// <param name="parameter">A parameter to be passed to the action.</param>
         public void Execute(T parameter)
         {
-            if (_staticAction != null)
+            if (StaticDelegate != null)
             {
-                _staticAction(parameter);
+                StaticDelegate.Invoke(parameter);
                 return;
             }
 
-            var actionTarget = ActionTarget;
-
-            if (IsAlive)
-            {
-                if (Method != null
-                    && ActionReference != null
-                    && actionTarget != null)
-                {
-                    Method.Invoke(
-                        actionTarget,
-                        new object[]
-                        {
-                            parameter
-                        });
-                }
-            }
+            TryExecuteWeakDelegate<object>(parameter);
         }
     }
 
