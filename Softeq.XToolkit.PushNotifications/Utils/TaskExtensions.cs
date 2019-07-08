148aa6e3bec1d766c3a451a23e214884cfed0ed3
Aleksandra Mashenkova <Aleksandra.Mashenkova@softeq.com>
2019-07-08 17:18:55 +0300

    remove unused code from temp copy of TaskExtensions

--- Softeq.XToolkit.PushNotifications/Utils/TaskExtensions.cs
+++ Softeq.XToolkit.PushNotifications/Utils/TaskExtensions.cs
@@ -8,28 +8,8 @@ using System.Threading.Tasks;
 namespace Softeq.XToolkit.PushNotifications.Utils
 {
     // TEMPORARY COPY
-    public static class TaskExtensions
+    internal static class TaskExtensions
     {
-        /// <summary>
-        ///     Task extension to add a timeout.
-        /// </summary>
-        /// <returns>The task with timeout.</returns>
-        /// <param name="task">Task.</param>
-        /// <param name="timeout"></param>
-        /// <typeparam name="T">The 1st type parameter.</typeparam>
-        public static async Task<T> WithTimeout<T>(this Task<T> task, TimeSpan timeout)
-        {
-            var retTask = await Task.WhenAny(task, Task.Delay((int) timeout.TotalMilliseconds))
-                .ConfigureAwait(false);
-
-            if (retTask is Task<T>)
-            {
-                return task.Result;
-            }
-
-            return default(T);
-        }
-
         public static Task<T> SafeTaskWrapper<T>(this Task<T> task)
         {
             task.ContinueWith(t => { LogException(t); }, CancellationToken.None,
@@ -39,7 +19,6 @@ namespace Softeq.XToolkit.PushNotifications.Utils
 
         public static Task SafeTaskWrapper(this Task task)
         {
-            //todo PL: implement default logger
             task.ContinueWith(t => { LogException(t); }, CancellationToken.None,
                 TaskContinuationOptions.NotOnRanToCompletion, TaskScheduler.Default);
             return task;
