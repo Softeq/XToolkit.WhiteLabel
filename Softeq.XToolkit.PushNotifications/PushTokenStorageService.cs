ee4c2ce3494348f6b3531de5cdb8d9832cd8a7bc
Aleksandra Mashenkova <Aleksandra.Mashenkova@softeq.com>
2019-07-09 12:58:34 +0300

    Add default IPushTokenStorageService implementation

---
+++ Softeq.XToolkit.PushNotifications/PushTokenStorageService.cs
@@ -0,0 +1,24 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+using Softeq.XToolkit.Common.Interfaces;
+
+namespace Softeq.XToolkit.PushNotifications
+{
+    public class PushTokenStorageService : IPushTokenStorageService
+    {
+        private readonly IInternalSettings _internalSettings;
+
+        public PushTokenStorageService(IInternalSettings internalSettings)
+        {
+            _internalSettings = internalSettings;
+        }
+
+        private const string PushTokenKey = "push_token_key";
+        public string PushToken
+        {
+            get => _internalSettings.GetValueOrDefault(PushTokenKey, default(string));
+            set => _internalSettings.AddOrUpdateValue(PushTokenKey, value);
+        }
+    }
+}
