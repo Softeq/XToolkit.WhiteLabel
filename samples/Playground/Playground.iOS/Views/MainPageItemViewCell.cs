a8849a2f42a647ec079e69ad08ededefbd3d8344
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2019-07-15 11:40:47 +0300

    Rework Playground Structure (#138)
    * fix
    
    * add playground main screen
    
    * Add missing files
    
    * Some cleanup
    
    * Remove storyborad for iOS tabbar sample
    
    * Rework Playgroup structure & cleanup
    
    * Resolve tableview headers warnings with bg
    
    * Update nav parameter

---
+++ samples/Playground/Playground.iOS/Views/MainPageItemViewCell.cs
@@ -0,0 +1,39 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+using System;
+using Foundation;
+using Softeq.XToolkit.Bindings.iOS.Bindable;
+using Softeq.XToolkit.WhiteLabel.Mvvm;
+using UIKit;
+
+namespace Playground.iOS.Views
+{
+    public partial class MainPageItemViewCell : BindableTableViewCell<CommandAction>
+    {
+        public static readonly NSString Key = new NSString(nameof(MainPageItemViewCell));
+        public static readonly UINib Nib;
+
+        static MainPageItemViewCell()
+        {
+            Nib = UINib.FromName(Key, NSBundle.MainBundle);
+        }
+
+        protected MainPageItemViewCell(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void AwakeFromNib()
+        {
+            base.AwakeFromNib();
+
+            SelectionStyle = UITableViewCellSelectionStyle.None;
+        }
+
+        public override void SetBindings()
+        {
+            NameLabel.Text = ViewModel.Title;
+        }
+    }
+}
---
+++ samples/Playground/Playground/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,128 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+using System.Collections.Generic;
+using Playground.Models;
+using Playground.ViewModels.BottomTabs;
+using Playground.ViewModels.Collections;
+using Playground.ViewModels.Components;
+using Playground.ViewModels.Pages;
+using Softeq.XToolkit.Common.Collections;
+using Softeq.XToolkit.Common.Command;
+using Softeq.XToolkit.WhiteLabel.Mvvm;
+using Softeq.XToolkit.WhiteLabel.Navigation;
+
+namespace Playground.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        private readonly IPageNavigationService _pageNavigationService;
+
+        public MainPageViewModel(
+            IPageNavigationService pageNavigationService)
+        {
+            _pageNavigationService = pageNavigationService;
+        }
+
+        public ObservableKeyGroupsCollection<string, CommandAction> Items { get; } =
+            new ObservableKeyGroupsCollection<string, CommandAction>();
+
+        public override void OnInitialize()
+        {
+            base.OnInitialize();
+
+            var actions = new List<(string Header, CommandAction Item)>
+            {
+                ("Navigation",
+                    new CommandAction
+                    {
+                        Title = "Without parameters",
+                        Command = new RelayCommand(() =>
+                        {
+                            _pageNavigationService
+                                .For<DetailsPageViewModel>()
+                                .Navigate();
+                        })
+                    }),
+                ("Navigation",
+                    new CommandAction
+                    {
+                        Title = "With parameters",
+                        Command = new RelayCommand(() =>
+                        {
+                            _pageNavigationService
+                                .For<DetailsPageViewModel>()
+                                .WithParam(x => x.Person, new Person
+                                {
+                                    FirstName = "Ivan",
+                                    LastName = "Ivanov"
+                                })
+                                .Navigate();
+                        })
+                    }),
+                ("Navigation",
+                    new CommandAction
+                    {
+                        Title = "Bottom Tabs",
+                        Command = new RelayCommand(() =>
+                        {
+                            _pageNavigationService
+                                .For<BottomTabsPageViewModel>()
+                                .Navigate();
+                        })
+                    }),
+                ("Collections",
+                    new CommandAction
+                    {
+                        Title = "Observable list",
+                        Command = new RelayCommand(() =>
+                        {
+                            _pageNavigationService
+                                .For<CollectionPageViewModel>()
+                                .Navigate();
+                        })
+                    }),
+                ("Collections",
+                    new CommandAction
+                    {
+                        Title = "Observable grouped list",
+                        Command = new RelayCommand(() =>
+                        {
+                            //_pageNavigationService.NavigateToViewModel<GrouppedCollectionViewModel>();
+                        })
+                    }),
+                ("Controls",
+                    new CommandAction
+                    {
+                        Title = "Photo browser",
+                        Command = new RelayCommand(() =>
+                        {
+                            //_pageNavigationService.NavigateToViewModel<PhotoBrowserViewModel>();
+                        })
+                    }),
+                ("Components",
+                    new CommandAction
+                    {
+                        Title = "Files",
+                        Command = new RelayCommand(() =>
+                        {
+                        //_pageNavigationService.NavigateToViewModel<FilesViewModel>();
+                        })
+                }),
+                ("Components",
+                    new CommandAction
+                    {
+                        Title = "Permissions",
+                        Command = new RelayCommand(() =>
+                        {
+                            _pageNavigationService
+                                .For<PermissionsPageViewModel>()
+                                .Navigate();
+                        })
+                    })
+            };
+
+            Items.AddRangeToGroups(actions, x => x.Item, x => x.Header);
+        }
+    }
+}
