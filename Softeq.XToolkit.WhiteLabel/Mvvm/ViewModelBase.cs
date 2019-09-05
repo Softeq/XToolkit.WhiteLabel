6d58d13fb6d639dfbb19c5e0e4dea5a022f86d74
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2019-09-05 12:26:34 +0300

    Cleanup ViewModelBase

--- Softeq.XToolkit.WhiteLabel/Mvvm/ViewModelBase.cs
+++ Softeq.XToolkit.WhiteLabel/Mvvm/ViewModelBase.cs
@@ -1,11 +1,6 @@
 ﻿// Developed by Softeq Development Corporation
 // http://www.softeq.com
 
-using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq.Expressions;
-using System.Runtime.CompilerServices;
 using Softeq.XToolkit.WhiteLabel.Navigation;
 
 namespace Softeq.XToolkit.WhiteLabel.Mvvm
@@ -17,14 +12,9 @@ namespace Softeq.XToolkit.WhiteLabel.Mvvm
         void OnDisappearing();
     }
 
-
     /// <summary>
     ///     A base class for the ViewModel classes in the MVVM pattern.
     /// </summary>
-    [SuppressMessage(
-        "Microsoft.Design",
-        "CA1012",
-        Justification = "Constructors should remain public to allow serialization.")]
     public abstract class ViewModelBase : ObservableObject, IViewModelBase
     {
         private bool _isBusy;
@@ -37,8 +27,10 @@ namespace Softeq.XToolkit.WhiteLabel.Mvvm
 
         public bool IsInitialized { get; private set; }
 
+        // TODO YP: Remove, use injection via ctor
         public IFrameNavigationService FrameNavigationService { get; set; }
 
+        // TODO YP: Review using lifetime methods here
         public virtual void OnInitialize()
         {
             IsInitialized = true;
@@ -51,132 +43,5 @@ namespace Softeq.XToolkit.WhiteLabel.Mvvm
         public virtual void OnDisappearing()
         {
         }
-
-        #region Property Change
-
-        // ReSharper disable PublicConstructorInAbstractClass
-
-        /// <summary>
-        ///     Raises the PropertyChanged event if needed, and broadcasts a
-        ///     PropertyChangedMessage using the Messenger instance (or the
-        ///     static default instance if no Messenger instance is available).
-        /// </summary>
-        /// <typeparam name="T">
-        ///     The type of the property that
-        ///     changed.
-        /// </typeparam>
-        /// <param name="propertyName">
-        ///     The name of the property that
-        ///     changed.
-        /// </param>
-        /// <param name="oldValue">
-        ///     The property's value before the change
-        ///     occurred.
-        /// </param>
-        /// <param name="newValue">
-        ///     The property's value after the change
-        ///     occurred.
-        /// </param>
-        [SuppressMessage(
-            "Microsoft.Design",
-            "CA1026:DefaultParametersShouldNotBeUsed")]
-        [SuppressMessage(
-            "Microsoft.Design", "CA1030:UseEventsWhereAppropriate",
-            Justification = "This cannot be an event")]
-        public virtual void RaisePropertyChanged<T>(
-            [CallerMemberName] string propertyName = null,
-            T oldValue = default,
-            T newValue = default)
-        {
-            if (string.IsNullOrEmpty(propertyName))
-            {
-                throw new ArgumentException("This method cannot be called with an empty string", nameof(propertyName));
-            }
-
-            // ReSharper disable ExplicitCallerInfoArgument
-            RaisePropertyChanged(propertyName);
-            // ReSharper restore ExplicitCallerInfoArgument
-        }
-
-        /// <summary>
-        ///     Raises the PropertyChanged event if needed, and broadcasts a
-        ///     PropertyChangedMessage using the Messenger instance (or the
-        ///     static default instance if no Messenger instance is available).
-        /// </summary>
-        /// <typeparam name="T">
-        ///     The type of the property that
-        ///     changed.
-        /// </typeparam>
-        /// <param name="propertyExpression">
-        ///     An expression identifying the property
-        ///     that changed.
-        /// </param>
-        /// <param name="oldValue">
-        ///     The property's value before the change
-        ///     occurred.
-        /// </param>
-        /// <param name="newValue">
-        ///     The property's value after the change
-        ///     occurred.
-        /// </param>
-        [SuppressMessage(
-            "Microsoft.Design",
-            "CA1030:UseEventsWhereAppropriate",
-            Justification = "This cannot be an event")]
-        [SuppressMessage(
-            "Microsoft.Design",
-            "CA1006:GenericMethodsShouldProvideTypeParameter",
-            Justification = "This syntax is more convenient than the alternatives.")]
-        public virtual void RaisePropertyChanged<T>(Expression<Func<T>> propertyExpression, T oldValue, T newValue)
-        {
-            RaisePropertyChanged(propertyExpression);
-        }
-
-        /// <summary>
-        ///     Assigns a new value to the property. Then, raises the
-        ///     PropertyChanged event if needed, and broadcasts a
-        ///     PropertyChangedMessage using the Messenger instance (or the
-        ///     static default instance if no Messenger instance is available).
-        /// </summary>
-        /// <typeparam name="T">
-        ///     The type of the property that
-        ///     changed.
-        /// </typeparam>
-        /// <param name="field">The field storing the property's value.</param>
-        /// <param name="newValue">
-        ///     The property's value after the change
-        ///     occurred.
-        /// </param>
-        /// <param name="broadcast">
-        ///     If true, a PropertyChangedMessage will
-        ///     be broadcasted. If false, only the event will be raised.
-        /// </param>
-        /// <param name="propertyName">
-        ///     (optional) The name of the property that
-        ///     changed.
-        /// </param>
-        /// <returns>True if the PropertyChanged event was raised, false otherwise.</returns>
-        protected bool Set<T>(
-            ref T field,
-            T newValue = default,
-            bool broadcast = false,
-            [CallerMemberName] string propertyName = null)
-        {
-            if (EqualityComparer<T>.Default.Equals(field, newValue))
-            {
-                return false;
-            }
-
-            var oldValue = field;
-            field = newValue;
-
-            // ReSharper disable ExplicitCallerInfoArgument
-            RaisePropertyChanged(propertyName, oldValue, field);
-            // ReSharper restore ExplicitCallerInfoArgument
-
-            return true;
-        }
-
-        #endregion Property Change
     }
 }
