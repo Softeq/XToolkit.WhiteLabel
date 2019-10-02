427aececc91787e1a9a148c204f86daddf9267ad
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2019-10-02 13:35:47 +0300

    Refactor console logger

--- Softeq.XToolkit.WhiteLabel/Services/Logger/ConsoleLogger.cs
+++ Softeq.XToolkit.WhiteLabel/Services/Logger/ConsoleLogger.cs
@@ -8,31 +8,44 @@ namespace Softeq.XToolkit.WhiteLabel.Services.Logger
 {
     public class ConsoleLogger : ILogger
     {
+        private readonly string _category;
+
+        public ConsoleLogger(string category)
+        {
+            _category = category;
+        }
+
+        /// <inheritdoc />
         public void Debug(string message)
         {
             WriteMessage(message);
         }
 
+        /// <inheritdoc />
         public void Info(string message)
         {
             WriteMessage(message);
         }
 
+        /// <inheritdoc />
         public void Warn(string message)
         {
             WriteMessage(message);
         }
 
+        /// <inheritdoc />
         public void Warn(Exception ex)
         {
             WriteMessage(ex);
         }
 
+        /// <inheritdoc />
         public void Error(string message)
         {
             WriteMessage(message);
         }
 
+        /// <inheritdoc />
         public void Error(Exception ex)
         {
             WriteMessage(ex);
@@ -40,13 +53,12 @@ namespace Softeq.XToolkit.WhiteLabel.Services.Logger
 
         private void WriteMessage(Exception ex)
         {
-            System.Diagnostics.Debug.WriteLine(ex.Message);
-            System.Diagnostics.Debug.WriteLine(ex.StackTrace);
+            System.Diagnostics.Debug.WriteLine(ex, _category);
         }
 
         private void WriteMessage(string message)
         {
-            System.Diagnostics.Debug.WriteLine(message);
+            System.Diagnostics.Debug.WriteLine(message, _category);
         }
     }
 }
