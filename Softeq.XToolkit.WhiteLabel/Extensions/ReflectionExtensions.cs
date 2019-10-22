7d65a8cc3e9709d4ab991721cd435eed3c020e21
Sergey Levshunov <sergeylevshunov@Sergeys-MacBook-Pro.local>
2019-10-22 12:42:52 +0300

    fix tabs

--- Softeq.XToolkit.WhiteLabel/Extensions/ReflectionExtensions.cs
+++ Softeq.XToolkit.WhiteLabel/Extensions/ReflectionExtensions.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Softeq.XToolkit.WhiteLabel.Mvvm;
 
 namespace Softeq.XToolkit.WhiteLabel.Extensions
 {
@@ -13,7 +14,8 @@ namespace Softeq.XToolkit.WhiteLabel.Extensions
         {
             return types.Where(type => viewEndWith.Any(type.IsSubclassOf) &&
                 type.BaseType != null && type.BaseType.IsGenericType &&
-                type.BaseType.GetGenericArguments()[0].AssemblyQualifiedName != null);
+                type.BaseType.GetGenericArguments()[0].AssemblyQualifiedName != null &&
+                type.BaseType.GetGenericArguments()[0].IsSubclassOf(typeof(ViewModelBase)));
         }
     }
 }
