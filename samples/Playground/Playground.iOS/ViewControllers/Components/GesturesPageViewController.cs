d558a9b7d1c2e7bbb999b9b93dafa8ad92053f80
Sergey Levshunov <sergeylevshunov@Sergeys-MacBook-Pro.local>
2019-11-14 17:00:20 +0300

    merge

---
+++ samples/Playground/Playground.iOS/ViewControllers/Components/GesturesPageViewController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using Playground.ViewModels.Components;
+using Softeq.XToolkit.Bindings.iOS.Gestures;
+using Softeq.XToolkit.Common.Commands;
+using Softeq.XToolkit.WhiteLabel.iOS;
+using UIKit;
+
+namespace Playground.iOS.ViewControllers.Components
+{
+    public partial class GesturesPageViewController : ViewControllerBase<GesturesPageViewModel>
+    {
+        public GesturesPageViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            TapViewContainer.Tap().Command = new AsyncCommand(async () =>
+            {
+               await HighlightView(TapViewContainer);
+            });
+
+            SwipeViewContainer.Swipe(UISwipeGestureRecognizerDirection.Right).Command = new AsyncCommand(async () =>
+            {
+                await HighlightView(SwipeViewContainer);
+            });
+
+            PanViewContainer.Pan().Command = new AsyncCommand(async () =>
+            {
+                await HighlightView(PanViewContainer);
+            });
+        }
+
+        private async Task HighlightView (UIView view)
+        {
+            view.BackgroundColor = UIColor.Red;
+            await Task.Delay(100);
+            view.BackgroundColor = UIColor.White;
+        }
+    }
+}
+
---
+++ samples/Playground/Playground/ViewModels/Components/GesturesPageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Softeq.XToolkit.WhiteLabel.Mvvm;
+
+namespace Playground.ViewModels.Components
+{
+    public class GesturesPageViewModel : ViewModelBase
+    {
+        public GesturesPageViewModel()
+        {
+        }
+
+
+    }
+}
