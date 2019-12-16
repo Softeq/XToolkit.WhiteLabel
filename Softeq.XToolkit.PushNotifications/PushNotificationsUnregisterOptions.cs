60361c55469121d83f6a5128674b8a8bbe3a87e2
AlexandraMash <aleksandra.mashenkova@softeq.com>
2019-12-16 16:53:54 +0300

    Push Notifications: Rework Unregister method a bit to allow unregiste… (#224)
    * Push Notifications: Rework Unregister method a bit to allow unregister in system only
    
    * PN: remove default parameter for new Unregister methos

---
+++ Softeq.XToolkit.PushNotifications/PushNotificationsUnregisterOptions.cs
@@ -0,0 +1,37 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+namespace Softeq.XToolkit.PushNotifications
+{
+    /// <summary>
+    /// Options of how to Unregister from Push Notifications
+    /// </summary>
+    public enum PushNotificationsUnregisterOptions
+    {
+        /// <summary>
+        /// Unregister in system only, but do not unregister on server
+        /// </summary>
+        InSystemOnly,
+
+        /// <summary>
+        /// Unregister on server only, but do not unregister in system
+        /// </summary>
+        OnServerOnly,
+
+        /// <summary>
+        /// Unregister both in system and on server
+        /// </summary>
+        InSystemAndOnServer
+    }
+
+    public static class PushNotificationsUnregisterOptionsExtensions
+    {
+        public static bool ShouldUnregisterInSystem(this PushNotificationsUnregisterOptions options) =>
+            options == PushNotificationsUnregisterOptions.InSystemOnly
+            || options == PushNotificationsUnregisterOptions.InSystemAndOnServer;
+
+        public static bool ShouldUnregisterOnServer(this PushNotificationsUnregisterOptions options) =>
+            options == PushNotificationsUnregisterOptions.OnServerOnly
+            || options == PushNotificationsUnregisterOptions.InSystemAndOnServer;
+    }
+}
