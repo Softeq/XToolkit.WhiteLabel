ffd6b26a9cefbdac3d0c1d9a9284edaf1b0d29a2
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-02-21 15:58:49 +0300

    Refactor tests by PR review.

--- Softeq.XToolkit.Common.Tests/Extensions/InternalSettingsExtensionsTests/InternalSettingsExtensionsTests.cs
+++ Softeq.XToolkit.Common.Tests/Extensions/InternalSettingsExtensionsTests/InternalSettingsExtensionsTests.cs
@@ -1,6 +1,7 @@
 ﻿// Developed by Softeq Development Corporation
 // http://www.softeq.com
 
+using System;
 using NSubstitute;
 using Softeq.XToolkit.Common.Extensions;
 using Softeq.XToolkit.Common.Interfaces;
@@ -8,38 +9,51 @@ using Xunit;
 
 namespace Softeq.XToolkit.Common.Tests.Extensions.InternalSettingsExtensionsTests
 {
-    public class InternalSettingsExtensionsTests
+    public class InternalSettingsExtensionsTests : IDisposable
     {
+        private readonly IInternalSettings _internalSettings;
+        private readonly IJsonSerializer _jsonSerializer;
+
+        private readonly string _key;
+        private readonly TestObject _nullValue;
+        private readonly TestObject _notNullValue;
+        private readonly string _notEmptyJson;
+
+        public InternalSettingsExtensionsTests()
+        {
+            _internalSettings = Substitute.For<IInternalSettings>();
+            _jsonSerializer = Substitute.For<IJsonSerializer>();
+
+            _key = "test_key";
+            _nullValue = null;
+            _notNullValue = new TestObject();
+            _notEmptyJson = "json";
+        }
+
+        public void Dispose()
+        {
+        }
+
         [Fact]
         public void AddOrUpdateJsonValue_NullValue_RemovesKey()
         {
-            var internalSettings = Substitute.For<IInternalSettings>();
-            var jsonSerializer = Substitute.For<IJsonSerializer>();
-            var key = InternalSettingsExtensionsHelper.Key;
-            var value = InternalSettingsExtensionsHelper.NullTestObject;
+            _internalSettings.AddOrUpdateJsonValue(_jsonSerializer, _key, _nullValue);
 
-            internalSettings.AddOrUpdateJsonValue(jsonSerializer, key, value);
-
-            internalSettings.Received().Remove(key);
-            jsonSerializer.DidNotReceive().Serialize(Arg.Any<InternalSettingsExtensionsHelper.TestObject>());
-            internalSettings.DidNotReceive().AddOrUpdateValue(Arg.Any<string>(), Arg.Any<string>());
+            _internalSettings.Received().Remove(_key);
+            _jsonSerializer.DidNotReceive().Serialize(Arg.Any<TestObject>());
+            _internalSettings.DidNotReceive().AddOrUpdateValue(Arg.Any<string>(), Arg.Any<string>());
         }
 
         [Fact]
         public void AddOrUpdateJsonValue_NotNullValue_AddsOrUpdatesKey()
         {
-            var internalSettings = Substitute.For<IInternalSettings>();
-            var jsonSerializer = Substitute.For<IJsonSerializer>();
-            var key = InternalSettingsExtensionsHelper.Key;
-            var value = InternalSettingsExtensionsHelper.NotNullTestObject;
-            var json = "json";
-            jsonSerializer.Serialize(value).Returns(json);
+            _jsonSerializer.Serialize(_notNullValue).Returns(_notEmptyJson);
 
-            internalSettings.AddOrUpdateJsonValue(jsonSerializer, key, value);
+            _internalSettings.AddOrUpdateJsonValue(_jsonSerializer, _key, _notNullValue);
 
-            jsonSerializer.Received().Serialize(value);
-            internalSettings.Received().AddOrUpdateValue(key, json);
-            internalSettings.DidNotReceive().Remove(Arg.Any<string>());
+            _jsonSerializer.Received().Serialize(_notNullValue);
+            _internalSettings.Received().AddOrUpdateValue(_key, _notEmptyJson);
+            _internalSettings.DidNotReceive().Remove(Arg.Any<string>());
         }
 
         [Theory]
@@ -47,17 +61,13 @@ namespace Softeq.XToolkit.Common.Tests.Extensions.InternalSettingsExtensionsTest
         [InlineData("")]
         public void GetJsonValueOrDefault_EmptyJsonAndDefaultNotSpecified_ReturnsDefaultValue(string json)
         {
-            var internalSettings = Substitute.For<IInternalSettings>();
-            var jsonSerializer = Substitute.For<IJsonSerializer>();
-            var key = InternalSettingsExtensionsHelper.Key;
-            var expectedValue = InternalSettingsExtensionsHelper.NullTestObject;
-            internalSettings.GetValueOrDefault(key, Arg.Any<string>()).Returns(json);
+            _internalSettings.GetValueOrDefault(_key, Arg.Any<string>()).Returns(json);
 
-            var value = internalSettings.GetJsonValueOrDefault<InternalSettingsExtensionsHelper.TestObject>(jsonSerializer, key);
+            var actualValue = _internalSettings.GetJsonValueOrDefault<TestObject>(_jsonSerializer, _key);
 
-            internalSettings.Received().GetValueOrDefault(key, default(string));
-            jsonSerializer.DidNotReceive().Deserialize<InternalSettingsExtensionsHelper.TestObject>(Arg.Any<string>());
-            Assert.Equal(expectedValue, value);
+            _internalSettings.Received().GetValueOrDefault(_key, default(string));
+            _jsonSerializer.DidNotReceive().Deserialize<TestObject>(Arg.Any<string>());
+            Assert.Equal(_nullValue, actualValue);
         }
 
         [Theory]
@@ -65,35 +75,26 @@ namespace Softeq.XToolkit.Common.Tests.Extensions.InternalSettingsExtensionsTest
         [InlineData("")]
         public void GetJsonValueOrDefault_EmptyJsonAndDefaultSpecified_ReturnsSpecifiedDefaultValue(string json)
         {
-            var internalSettings = Substitute.For<IInternalSettings>();
-            var jsonSerializer = Substitute.For<IJsonSerializer>();
-            var key = InternalSettingsExtensionsHelper.Key;
-            var expectedValue = InternalSettingsExtensionsHelper.NotNullTestObject;
-            internalSettings.GetValueOrDefault(key, Arg.Any<string>()).Returns(json);
+            _internalSettings.GetValueOrDefault(_key, Arg.Any<string>()).Returns(json);
 
-            var value = internalSettings.GetJsonValueOrDefault(jsonSerializer, key, expectedValue);
+            var actualValue = _internalSettings.GetJsonValueOrDefault(_jsonSerializer, _key, _nullValue);
 
-            internalSettings.Received().GetValueOrDefault(key, default(string));
-            jsonSerializer.DidNotReceive().Deserialize<InternalSettingsExtensionsHelper.TestObject>(Arg.Any<string>());
-            Assert.Equal(expectedValue, value);
+            _internalSettings.Received().GetValueOrDefault(_key, default(string));
+            _jsonSerializer.DidNotReceive().Deserialize<TestObject>(Arg.Any<string>());
+            Assert.Equal(_nullValue, actualValue);
         }
 
         [Fact]
         public void GetJsonValueOrDefault_NotEmptyJson_ReturnsSerializedValue()
         {
-            var internalSettings = Substitute.For<IInternalSettings>();
-            var jsonSerializer = Substitute.For<IJsonSerializer>();
-            var key = InternalSettingsExtensionsHelper.Key;
-            var expectedValue = InternalSettingsExtensionsHelper.NotNullTestObject;
-            var json = "json";
-            internalSettings.GetValueOrDefault(key, Arg.Any<string>()).Returns(json);
-            jsonSerializer.Deserialize<InternalSettingsExtensionsHelper.TestObject>(json).Returns(expectedValue);
+            _internalSettings.GetValueOrDefault(_key, Arg.Any<string>()).Returns(_notEmptyJson);
+            _jsonSerializer.Deserialize<TestObject>(_notEmptyJson).Returns(_nullValue);
 
-            var value = internalSettings.GetJsonValueOrDefault<InternalSettingsExtensionsHelper.TestObject>(jsonSerializer, key);
+            var actualValue = _internalSettings.GetJsonValueOrDefault<TestObject>(_jsonSerializer, _key);
 
-            internalSettings.Received().GetValueOrDefault(key, default(string));
-            jsonSerializer.Received().Deserialize<InternalSettingsExtensionsHelper.TestObject>(json);
-            Assert.Equal(expectedValue, value);
+            _internalSettings.Received().GetValueOrDefault(_key, default(string));
+            _jsonSerializer.Received().Deserialize<TestObject>(_notEmptyJson);
+            Assert.Equal(_nullValue, actualValue);
         }
     }
 }
