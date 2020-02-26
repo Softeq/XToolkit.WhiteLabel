20ac15b1006b6e56fe8772be0a7e7f1f0256d22a
Sergey Levshunov <sergeylevshunov@Sergeys-MacBook-Pro.local>
2020-02-26 12:23:16 +0300

    remove static dependencies

--- Softeq.XToolkit.WhiteLabel/Model/TabItem.cs
+++ Softeq.XToolkit.WhiteLabel/Model/TabItem.cs
@@ -1,6 +1,7 @@
 // Developed by Softeq Development Corporation
 // http://www.softeq.com
 
+using Softeq.XToolkit.WhiteLabel.Bootstrapper.Abstract;
 using Softeq.XToolkit.WhiteLabel.Mvvm;
 using Softeq.XToolkit.WhiteLabel.Navigation;
 using Softeq.XToolkit.WhiteLabel.ViewModels.Tab;
@@ -24,13 +25,16 @@ namespace Softeq.XToolkit.WhiteLabel.Model
 
     public class TabItem<TFirstViewModel, TKey> : TabItem<TKey> where TFirstViewModel : ViewModelBase
     {
-        public TabItem(string title, TKey key) : base(title, key)
+        public TabItem(string title, TKey key, IContainer container) : base(title, key)
         {
+            Container = container;
         }
+
+        protected IContainer Container { get; }
 
         public override TabViewModel<TKey> CreateViewModel()
         {
-            var frameNavigationService = Dependencies.Container.Resolve<IFrameNavigationService>();
+            var frameNavigationService = Container.Resolve<IFrameNavigationService>();
             var tabViewModel = new TabViewModel<TFirstViewModel, TKey>(frameNavigationService);
             tabViewModel.Initialize(this);
             return tabViewModel;
