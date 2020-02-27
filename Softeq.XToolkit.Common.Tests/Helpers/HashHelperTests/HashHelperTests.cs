73becae4bfd3b1420ab7c7f407df9c3c1d451db2
Timofey Burak <timofey.burak@softeq.com>
2020-02-27 11:11:43 +0300

    Implement unified GetHashCode method with null-values support. Implement null-values tests

--- Softeq.XToolkit.Common.Tests/Helpers/HashHelperTests/HashHelperTests.cs
+++ Softeq.XToolkit.Common.Tests/Helpers/HashHelperTests/HashHelperTests.cs
@@ -10,7 +10,14 @@ namespace Softeq.XToolkit.Common.Tests.Helpers.HashHelperTests
     {
         [Theory]
         [MemberData(nameof(HashHelperDataProvider.Hash2ArgumentsData), MemberType = typeof(HashHelperDataProvider))]
-        public void GetHashHode_2Arguments_Persists(object arg1, object arg2)
+        public void GetHashCode_NullArguments_DoesNotThrow(object arg1, object arg2)
+        {
+            HashHelper.GetHashCode(null, arg1, arg2);
+        }
+
+        [Theory]
+        [InlineData(null, null)]
+        public void GetHashCode_AllNullArguments_Persists(object arg1, object arg2)
         {
             var hash1 = HashHelper.GetHashCode(arg1, arg2);
             var hash2 = HashHelper.GetHashCode(arg1, arg2);
@@ -20,7 +27,17 @@ namespace Softeq.XToolkit.Common.Tests.Helpers.HashHelperTests
 
         [Theory]
         [MemberData(nameof(HashHelperDataProvider.Hash2ArgumentsData), MemberType = typeof(HashHelperDataProvider))]
-        public void GetHashHode_2Arguments_ReverseOrderDiffers(object arg1, object arg2)
+        public void GetHashCode_2Arguments_Persists(object arg1, object arg2)
+        {
+            var hash1 = HashHelper.GetHashCode(arg1, arg2);
+            var hash2 = HashHelper.GetHashCode(arg1, arg2);
+
+            Assert.Equal(hash1, hash2);
+        }
+
+        [Theory]
+        [MemberData(nameof(HashHelperDataProvider.Hash2ArgumentsData), MemberType = typeof(HashHelperDataProvider))]
+        public void GetHashCode_2Arguments_ReverseOrderDiffers(object arg1, object arg2)
         {
             var hash1 = HashHelper.GetHashCode(arg1, arg2);
             var hash2 = HashHelper.GetHashCode(arg2, arg1);
@@ -30,7 +47,7 @@ namespace Softeq.XToolkit.Common.Tests.Helpers.HashHelperTests
 
         [Theory]
         [MemberData(nameof(HashHelperDataProvider.Hash3ArgumentsData), MemberType = typeof(HashHelperDataProvider))]
-        public void GetHashHode_3Arguments_Persists(object arg1, object arg2, object arg3)
+        public void GetHashCode_3Arguments_Persists(object arg1, object arg2, object arg3)
         {
             var hash1 = HashHelper.GetHashCode(arg1, arg2, arg3);
             var hash2 = HashHelper.GetHashCode(arg1, arg2, arg3);
@@ -40,7 +57,7 @@ namespace Softeq.XToolkit.Common.Tests.Helpers.HashHelperTests
 
         [Theory]
         [MemberData(nameof(HashHelperDataProvider.Hash3ArgumentsData), MemberType = typeof(HashHelperDataProvider))]
-        public void GetHashHode_3Arguments_ReverseOrderDiffers(object arg1, object arg2, object arg3)
+        public void GetHashCode_3Arguments_ReverseOrderDiffers(object arg1, object arg2, object arg3)
         {
             var hash1 = HashHelper.GetHashCode(arg1, arg2, arg3);
             var hash2 = HashHelper.GetHashCode(arg3, arg2, arg1);
@@ -50,7 +67,7 @@ namespace Softeq.XToolkit.Common.Tests.Helpers.HashHelperTests
 
         [Theory]
         [MemberData(nameof(HashHelperDataProvider.Hash4ArgumentsData), MemberType = typeof(HashHelperDataProvider))]
-        public void GetHashHode_4Arguments_Persists(object arg1, object arg2, object arg3, object arg4)
+        public void GetHashCode_4Arguments_Persists(object arg1, object arg2, object arg3, object arg4)
         {
             var hash1 = HashHelper.GetHashCode(arg1, arg2, arg3, arg4);
             var hash2 = HashHelper.GetHashCode(arg1, arg2, arg3, arg4);
@@ -60,7 +77,7 @@ namespace Softeq.XToolkit.Common.Tests.Helpers.HashHelperTests
 
         [Theory]
         [MemberData(nameof(HashHelperDataProvider.Hash4ArgumentsData), MemberType = typeof(HashHelperDataProvider))]
-        public void GetHashHode_4Arguments_ReverseOrderDiffers(object arg1, object arg2, object arg3, object arg4)
+        public void GetHashCode_4Arguments_ReverseOrderDiffers(object arg1, object arg2, object arg3, object arg4)
         {
             var hash1 = HashHelper.GetHashCode(arg1, arg2, arg3, arg4);
             var hash2 = HashHelper.GetHashCode(arg4, arg3, arg2, arg1);
@@ -70,7 +87,7 @@ namespace Softeq.XToolkit.Common.Tests.Helpers.HashHelperTests
 
         [Theory]
         [MemberData(nameof(HashHelperDataProvider.Hash5ArgumentsData), MemberType = typeof(HashHelperDataProvider))]
-        public void GetHashHode_5Arguments_Persists(object arg1, object arg2, object arg3, object arg4, object arg5)
+        public void GetHashCode_5Arguments_Persists(object arg1, object arg2, object arg3, object arg4, object arg5)
         {
             var hash1 = HashHelper.GetHashCode(arg1, arg2, arg3, arg4, arg5);
             var hash2 = HashHelper.GetHashCode(arg1, arg2, arg3, arg4, arg5);
@@ -80,7 +97,7 @@ namespace Softeq.XToolkit.Common.Tests.Helpers.HashHelperTests
 
         [Theory]
         [MemberData(nameof(HashHelperDataProvider.Hash5ArgumentsData), MemberType = typeof(HashHelperDataProvider))]
-        public void GetHashHode_5Arguments_ReverseOrderDiffers(object arg1, object arg2, object arg3, object arg4, object arg5)
+        public void GetHashCode_5Arguments_ReverseOrderDiffers(object arg1, object arg2, object arg3, object arg4, object arg5)
         {
             var hash1 = HashHelper.GetHashCode(arg1, arg2, arg3, arg4, arg5);
             var hash2 = HashHelper.GetHashCode(arg5, arg4, arg3, arg2, arg1);
@@ -90,7 +107,7 @@ namespace Softeq.XToolkit.Common.Tests.Helpers.HashHelperTests
 
         [Theory]
         [MemberData(nameof(HashHelperDataProvider.Hash6ArgumentsData), MemberType = typeof(HashHelperDataProvider))]
-        public void GetHashHode_6Arguments_Persists(object arg1, object arg2, object arg3, object arg4, object arg5, object arg6)
+        public void GetHashCode_6Arguments_Persists(object arg1, object arg2, object arg3, object arg4, object arg5, object arg6)
         {
             var hash1 = HashHelper.GetHashCode(arg1, arg2, arg3, arg4, arg5, arg6);
             var hash2 = HashHelper.GetHashCode(arg1, arg2, arg3, arg4, arg5, arg6);
@@ -100,7 +117,7 @@ namespace Softeq.XToolkit.Common.Tests.Helpers.HashHelperTests
 
         [Theory]
         [MemberData(nameof(HashHelperDataProvider.Hash6ArgumentsData), MemberType = typeof(HashHelperDataProvider))]
-        public void GetHashHode_6Arguments_ReverseOrderDiffers(object arg1, object arg2, object arg3, object arg4, object arg5,
+        public void GetHashCode_6Arguments_ReverseOrderDiffers(object arg1, object arg2, object arg3, object arg4, object arg5,
             object arg6)
         {
             var hash1 = HashHelper.GetHashCode(arg1, arg2, arg3, arg4, arg5, arg6);
@@ -111,7 +128,7 @@ namespace Softeq.XToolkit.Common.Tests.Helpers.HashHelperTests
 
         [Theory]
         [MemberData(nameof(HashHelperDataProvider.Hash7ArgumentsData), MemberType = typeof(HashHelperDataProvider))]
-        public void GetHashHode_7Arguments_Persists(object arg1, object arg2, object arg3, object arg4, object arg5, object arg6,
+        public void GetHashCode_7Arguments_Persists(object arg1, object arg2, object arg3, object arg4, object arg5, object arg6,
             object arg7)
         {
             var hash1 = HashHelper.GetHashCode(arg1, arg2, arg3, arg4, arg5, arg6, arg7);
@@ -122,7 +139,7 @@ namespace Softeq.XToolkit.Common.Tests.Helpers.HashHelperTests
 
         [Theory]
         [MemberData(nameof(HashHelperDataProvider.Hash7ArgumentsData), MemberType = typeof(HashHelperDataProvider))]
-        public void GetHashHode_7Arguments_ReverseOrderDiffers(object arg1, object arg2, object arg3, object arg4, object arg5,
+        public void GetHashCode_7Arguments_ReverseOrderDiffers(object arg1, object arg2, object arg3, object arg4, object arg5,
             object arg6, object arg7)
         {
             var hash1 = HashHelper.GetHashCode(arg1, arg2, arg3, arg4, arg5, arg6, arg7);
@@ -133,7 +150,7 @@ namespace Softeq.XToolkit.Common.Tests.Helpers.HashHelperTests
 
         [Theory]
         [MemberData(nameof(HashHelperDataProvider.Hash8ArgumentsData), MemberType = typeof(HashHelperDataProvider))]
-        public void GetHashHode_8Arguments_Persists(object arg1, object arg2, object arg3, object arg4, object arg5, object arg6,
+        public void GetHashCode_8Arguments_Persists(object arg1, object arg2, object arg3, object arg4, object arg5, object arg6,
             object arg7, object arg8)
         {
             var hash1 = HashHelper.GetHashCode(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8);
@@ -144,7 +161,7 @@ namespace Softeq.XToolkit.Common.Tests.Helpers.HashHelperTests
 
         [Theory]
         [MemberData(nameof(HashHelperDataProvider.Hash8ArgumentsData), MemberType = typeof(HashHelperDataProvider))]
-        public void GetHashHode_8Arguments_ReverseOrderDiffers(object arg1, object arg2, object arg3, object arg4, object arg5,
+        public void GetHashCode_8Arguments_ReverseOrderDiffers(object arg1, object arg2, object arg3, object arg4, object arg5,
             object arg6, object arg7, object arg8)
         {
             var hash1 = HashHelper.GetHashCode(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8);
@@ -155,7 +172,7 @@ namespace Softeq.XToolkit.Common.Tests.Helpers.HashHelperTests
 
         [Theory]
         [MemberData(nameof(HashHelperDataProvider.Hash9ArgumentsData), MemberType = typeof(HashHelperDataProvider))]
-        public void GetHashHode_9Arguments_Persists(object arg1, object arg2, object arg3, object arg4, object arg5, object arg6,
+        public void GetHashCode_9Arguments_Persists(object arg1, object arg2, object arg3, object arg4, object arg5, object arg6,
             object arg7, object arg8, object arg9)
         {
             var hash1 = HashHelper.GetHashCode(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9);
@@ -166,7 +183,7 @@ namespace Softeq.XToolkit.Common.Tests.Helpers.HashHelperTests
 
         [Theory]
         [MemberData(nameof(HashHelperDataProvider.Hash9ArgumentsData), MemberType = typeof(HashHelperDataProvider))]
-        public void GetHashHode_9Arguments_ReverseOrderDiffers(object arg1, object arg2, object arg3, object arg4, object arg5,
+        public void GetHashCode_9Arguments_ReverseOrderDiffers(object arg1, object arg2, object arg3, object arg4, object arg5,
             object arg6, object arg7, object arg8, object arg9)
         {
             var hash1 = HashHelper.GetHashCode(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9);
@@ -177,7 +194,7 @@ namespace Softeq.XToolkit.Common.Tests.Helpers.HashHelperTests
 
         [Theory]
         [MemberData(nameof(HashHelperDataProvider.Hash10ArgumentsData), MemberType = typeof(HashHelperDataProvider))]
-        public void GetHashHode_10Arguments_Persists(object arg1, object arg2, object arg3, object arg4, object arg5, object arg6,
+        public void GetHashCode_10Arguments_Persists(object arg1, object arg2, object arg3, object arg4, object arg5, object arg6,
             object arg7, object arg8, object arg9, object arg10)
         {
             var hash1 = HashHelper.GetHashCode(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10);
@@ -188,7 +205,7 @@ namespace Softeq.XToolkit.Common.Tests.Helpers.HashHelperTests
 
         [Theory]
         [MemberData(nameof(HashHelperDataProvider.Hash10ArgumentsData), MemberType = typeof(HashHelperDataProvider))]
-        public void GetHashHode_10Arguments_ReverseOrderDiffers(object arg1, object arg2, object arg3, object arg4, object arg5,
+        public void GetHashCode_10Arguments_ReverseOrderDiffers(object arg1, object arg2, object arg3, object arg4, object arg5,
             object arg6, object arg7, object arg8, object arg9, object arg10)
         {
             var hash1 = HashHelper.GetHashCode(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10);
