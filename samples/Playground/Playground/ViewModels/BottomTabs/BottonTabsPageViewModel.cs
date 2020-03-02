0836b000ab6381ea3755b229dbdfeb1adfc055a6
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-03-02 12:41:29 +0300

    Revert "fix playground"
    This reverts commit aeff88adcc7f6720669bf081d485cef5b34f1b72.

--- samples/Playground/Playground/ViewModels/BottomTabs/BottonTabsPageViewModel.cs
+++ samples/Playground/Playground/ViewModels/BottomTabs/BottonTabsPageViewModel.cs
@@ -4,6 +4,7 @@
 using System.Collections.Generic;
 using Playground.ViewModels.BottomTabs.First;
 using Playground.ViewModels.BottomTabs.Second;
+using Softeq.XToolkit.WhiteLabel.Bootstrapper.Abstract;
 using Softeq.XToolkit.WhiteLabel.Model;
 using Softeq.XToolkit.WhiteLabel.ViewModels.Tab;
 
@@ -11,12 +12,12 @@ namespace Playground.ViewModels.BottomTabs
 {
     public class BottomTabsPageViewModel : ToolbarViewModelBase<string>
     {
-        public BottomTabsPageViewModel()
+        public BottomTabsPageViewModel(IContainer container)
         {
             TabModels = new List<TabItem<string>>
             {
-                new TabItem<RedViewModel, string>("First", "Chat"),
-                new TabItem<BlueViewModel, string>("Second", "Settings")
+                new TabItem<RedViewModel, string>("First", "Chat", container),
+                new TabItem<BlueViewModel, string>("Second", "Settings", container)
             };
         }
     }
