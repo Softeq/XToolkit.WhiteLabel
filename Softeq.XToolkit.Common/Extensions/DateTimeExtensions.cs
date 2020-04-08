0ad8c54e4baef980bd983d673e1220423f62aacb
savikpavel <pavel.savik@softeq.com>
2020-04-08 14:40:06 +0300

    Documentation cleanup for DateTime and Dictionary extensions

--- Softeq.XToolkit.Common/Extensions/DateTimeExtensions.cs
+++ Softeq.XToolkit.Common/Extensions/DateTimeExtensions.cs
@@ -7,92 +7,95 @@ namespace Softeq.XToolkit.Common.Extensions
 {
     public static class DateTimeExtensions
     {
-        /// <summary> Standard amount of days in a week </summary>
+        /// <summary> Standard amount of days in a week.</summary>
         public const int DaysInWeek = 7;
 
         /// <summary>
-        /// Finds the date of the first day in week of the given date
+        /// Finds the date of the first day in week of the given date.
         /// </summary>
-        /// <param name="date">Date for which the start of the week is calculated</param>
-        /// <param name="startDayOfWeek">DayOfWeek value of the start of the week. Default is Sunday</param>
-        /// <returns>DateTime of the first day in required week</returns>
+        /// <param name="date">Date for which the start of the week is calculated.</param>
+        /// <param name="startDayOfWeek">DayOfWeek value of the start of the week. Default is Sunday.</param>
+        /// <returns>DateTime of the first day in required week.</returns>
         public static DateTime FirstDayOfWeek(this DateTime date, DayOfWeek startDayOfWeek = System.DayOfWeek.Sunday)
         {
             int diff = (DaysInWeek + (date.DayOfWeek - startDayOfWeek)) % DaysInWeek;
+
             return date.AddDays(-diff).Date;
         }
 
-        /// <summary> Finds the date of the last day in week of the given date </summary>
-        /// <param name="date">Date for which the end of the week is calculated</param>
-        /// <param name="endDayOfWeek">DayOfWeek value of the end of the week. Default is Saturday</param>
-        /// <returns>DateTime of the last day in required week</returns>
+        /// <summary> Finds the date of the last day in week of the given date.</summary>
+        /// <param name="date">Date for which the end of the week is calculated.</param>
+        /// <param name="endDayOfWeek">DayOfWeek value of the end of the week. Default is Saturday.</param>
+        /// <returns>DateTime of the last day in required week.</returns>
         public static DateTime LastDayOfWeek(this DateTime date, DayOfWeek endDayOfWeek = System.DayOfWeek.Saturday)
         {
             int diff = (DaysInWeek + (endDayOfWeek - date.DayOfWeek)) % DaysInWeek;
+
             return date.AddDays(diff).Date;
         }
 
         /// <summary>
-        /// Finds the date of the first day in month of the given date
+        /// Finds the date of the first day in month of the given date.
         /// </summary>
-        /// <param name="date">Date for which the start of the month is calculated</param>
-        /// <returns>DateTime of the first day in required month</returns>
-        public static DateTime FirstDayOfMonth(this DateTime dateTime)
+        /// <param name="date">Date for which the start of the month is calculated.</param>
+        /// <returns>DateTime of the first day in required month.</returns>
+        public static DateTime FirstDayOfMonth(this DateTime date)
         {
-            return dateTime.AddDays(-dateTime.Day + 1).Date;
+            return date.AddDays(-date.Day + 1).Date;
         }
 
         /// <summary>
-        /// Finds the date of the last day in month of the given date
+        /// Finds the date of the last day in month of the given date.
         /// </summary>
-        /// <param name="date">Date for which the end of the month is calculated</param>
-        /// <returns>DateTime of the last day in required month</returns>
-        public static DateTime LastDayOfMonth(this DateTime dateTime)
+        /// <param name="date">Date for which the end of the month is calculated.</param>
+        /// <returns>DateTime of the last day in required month.</returns>
+        public static DateTime LastDayOfMonth(this DateTime date)
         {
-            var nextMonthDate = dateTime.AddMonths(1);
+            var nextMonthDate = date.AddMonths(1);
+
             return nextMonthDate.AddDays(-nextMonthDate.Day).Date;
         }
 
-        /// <summary> Determines if the given date is today </summary>
-        /// <param name="date">Date to check if it is today</param>
-        /// <returns><c>true</c> if the date is today, <c>false</c> otherwise</returns>
+        /// <summary> Determines if the given date is today.</summary>
+        /// <param name="date">Date to check if it is today.</param>
+        /// <returns><c>true</c> if the date is today, <c>false</c> otherwise.</returns>
         public static bool IsToday(this DateTime date)
         {
             return DateTime.Today == date.Date;
         }
 
-        /// <summary> Determines if the given date was yesterday </summary>
-        /// <param name="date">Date to check if it was yesterday</param>
-        /// <returns><c>true</c> if the date was yesterday, <c>false</c> otherwise</returns>
+        /// <summary> Determines if the given date was yesterday.</summary>
+        /// <param name="date">Date to check if it was yesterday.</param>
+        /// <returns><c>true</c> if the date was yesterday, <c>false</c> otherwise.</returns>
         public static bool IsYesterday(this DateTime date)
         {
             return (DateTime.Today - date.Date).Days == 1;
         }
 
         // TODO: add unit tests for methods below
+
         /// <summary>
-        ///     Determines if the given date is included in current week. You can specify the start and the length of the week
+        /// Determines if the given date is included in current week. You can specify the start and the length of the week.
         /// </summary>
-        /// <param name="date">Date to check if in current week</param>
-        /// <param name="startDayOfWeek">DayOfWeek value of the start of the week. Default is Sunday</param>
-        /// <param name="weekLength">Length of the week. Default is 7</param>
-        /// <returns><c>true</c> if the date is in current week, <c>false</c> otherwise</returns>
-        public static bool IsInCurrentWeek(this DateTime date, DayOfWeek startDayOfWeek = System.DayOfWeek.Sunday,
-            int weekLength = DaysInWeek)
+        /// <param name="date">Date to check if in current week.</param>
+        /// <param name="startDayOfWeek">DayOfWeek value of the start of the week. Default is Sunday.</param>
+        /// <param name="weekLength">Length of the week. Default is 7.</param>
+        /// <returns><c>true</c> if the date is in current week, <c>false</c> otherwise.</returns>
+        public static bool IsInCurrentWeek(this DateTime date, DayOfWeek startDayOfWeek = System.DayOfWeek.Sunday, int weekLength = DaysInWeek)
         {
             var startOfWeek = DateTime.Today.FirstDayOfWeek(startDayOfWeek);
+
             return date.Date >= startOfWeek && date.Date < startOfWeek.AddDays(weekLength);
         }
 
         /// <summary>
-        ///     Finds the number of this date day in a week. You can specify the start and the length of the week
+        /// Finds the index of this date day in a week. You can specify the start and the length of the week.
         /// </summary>
-        /// <param name="date">Date to find the day number</param>
-        /// <param name="startDayOfWeek">DayOfWeek value of the start of the week. Default is Sunday</param>
-        /// <param name="weekLength">Length of the week. Default is 7</param>
-        /// <returns></returns>
-        public static int DayOfWeek(this DateTime date, DayOfWeek startDayOfWeek = System.DayOfWeek.Sunday,
-            int weekLength = DaysInWeek)
+        /// <param name="date">Date to find the day number.</param>
+        /// <param name="startDayOfWeek">DayOfWeek value of the start of the week. Default is Sunday.</param>
+        /// <param name="weekLength">Length of the week. Default is 7.</param>
+        /// <returns>The index of the specified day in the week.</returns>
+        public static int DayOfWeek(this DateTime date, DayOfWeek startDayOfWeek = System.DayOfWeek.Sunday, int weekLength = DaysInWeek)
         {
             var dayOfWeek = ((int) date.DayOfWeek - (int) startDayOfWeek + weekLength) % weekLength;
 
--- Softeq.XToolkit.Common/Extensions/DictionaryExtensions.cs
+++ Softeq.XToolkit.Common/Extensions/DictionaryExtensions.cs
@@ -9,7 +9,7 @@ namespace Softeq.XToolkit.Common.Extensions
     public static class DictionaryExtensions
     {
         /// <summary>
-        ///     Gets the value for a key. If the key does not exist, return default(TValue);
+        ///     Gets the value for a key. If the key does not exist, return default(TValue).
         /// </summary>
         /// <typeparam name="TKey">The type of the keys in the dictionary.</typeparam>
         /// <typeparam name="TValue">The type of the values in the dictionary.</typeparam>
@@ -24,13 +24,13 @@ namespace Softeq.XToolkit.Common.Extensions
 
         /// <summary>
         ///     Adds value with a specified key to the dictionary.
-        ///     If the key already exists in the dictionary, replaces the value for this key
+        ///     If the key already exists in the dictionary, replaces the value for this key.
         /// </summary>
         /// <typeparam name="TKey">The type of the keys in the dictionary.</typeparam>
         /// <typeparam name="TValue">The type of the values in the dictionary.</typeparam>
         /// <param name="dictionary">The dictionary to call this method on.</param>
         /// <param name="key">The key to look up.</param>
-        /// <param name="value">The value to add to the dictionary</param>
+        /// <param name="value">The value to add to the dictionary.</param>
         public static void AddOrReplace<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue value)
         {
             if (dictionary.ContainsKey(key))
