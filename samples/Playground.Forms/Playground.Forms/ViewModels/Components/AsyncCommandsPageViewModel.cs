557f69a8875596eeec87cd230aca59d6e4b89f79
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-04-10 17:28:22 +0300

    Update sample

--- samples/Playground.Forms/Playground.Forms/ViewModels/Components/AsyncCommandsPageViewModel.cs
+++ samples/Playground.Forms/Playground.Forms/ViewModels/Components/AsyncCommandsPageViewModel.cs
@@ -1,27 +1,41 @@
 // Developed by Softeq Development Corporation
 // http://www.softeq.com
 
+using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading;
 using System.Threading.Tasks;
 using Softeq.XToolkit.Common.Commands;
+using Softeq.XToolkit.WhiteLabel.Dialogs;
 using Softeq.XToolkit.WhiteLabel.Mvvm;
+using Softeq.XToolkit.WhiteLabel.Navigation;
+using Softeq.XToolkit.WhiteLabel.Threading;
 
 namespace Playground.Forms.ViewModels.Components
 {
+    [SuppressMessage("ReSharper", "ArgumentsStyleNamedExpression", Justification = "Used for sample")]
+    [SuppressMessage("ReSharper", "ArgumentsStyleAnonymousFunction", Justification = "Used for sample")]
     public class AsyncCommandsPageViewModel : ViewModelBase
     {
+        private readonly IDialogsService _dialogsService;
+
         private CancellationTokenSource _cts = new CancellationTokenSource();
         private bool _inProgress;
 
-        public AsyncCommandsPageViewModel()
+        public AsyncCommandsPageViewModel(
+            IDialogsService dialogsService)
         {
+            _dialogsService = dialogsService;
+
             WorkCommand = new AsyncCommand<int>(
                 execute: DoWork,
-                canExecute: () => !InProgress);
+                canExecute: _ => !InProgress,
+                onException: ErrorHandler);
 
             CancelCommand = new AsyncCommand(
                 execute: Cancel,
-                canExecute: () => InProgress);
+                canExecute: () => InProgress,
+                onException: ErrorHandler);
         }
 
         public bool InProgress
@@ -49,9 +63,6 @@ namespace Playground.Forms.ViewModels.Components
             {
                 await Task.Delay(value * 1000, _cts.Token);
             }
-            catch (TaskCanceledException)
-            {
-            }
             finally
             {
                 InProgress = false;
@@ -63,6 +74,15 @@ namespace Playground.Forms.ViewModels.Components
             Interlocked.Exchange(ref _cts, new CancellationTokenSource()).Cancel();
 
             return Task.CompletedTask;
+        }
+
+        private async void ErrorHandler(Exception ex)
+        {
+            await Execute.OnUIThreadAsync(async () =>
+            {
+                await _dialogsService.ShowDialogAsync(
+                    new AlertDialogConfig("Exception", ex.Message, "OK"));
+            });
         }
     }
 }
