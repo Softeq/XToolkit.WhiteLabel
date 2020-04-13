9f261c46f946968d3f1dd10b3b81b8e1d3b2c9ef
AlexandraMash <aleksandra.mashenkova@softeq.com>
2020-04-13 21:09:38 +0300

    Add some converters and controls to WhiteLabel.Forms (#291)
    * Add some converters and controls
    
    * Xamarin.Forms: updated converters to be more universal and added some use cases description
    
    * Update Softeq.XToolkit.WhiteLabel.Forms/Converters/IsNumberZeroConverter.cs
    
    Co-Authored-By: Yauheni Pakala <evgeniy.pakalo@gmail.com>
    
    Co-authored-by: Yauheni Pakala <evgeniy.pakalo@gmail.com>

---
+++ Softeq.XToolkit.WhiteLabel.Forms/Converters/StringToLowerConverter.cs
@@ -0,0 +1,26 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Softeq.XToolkit.WhiteLabel.Forms.Converters
+{
+    /// <summary>
+    ///     Converts a string to LowerCase string.
+    ///     ConvertBack is not implemented.
+    /// </summary>
+    public class StringToLowerConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value as string)?.ToLower() ?? string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
