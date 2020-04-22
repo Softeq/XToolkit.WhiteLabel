5fcebc0db5117780e4a9797d475a72f3a31c7aa8
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-04-22 17:12:37 +0300

    Refactor playground connectivity sample

--- samples/Playground/Playground/ViewModels/Components/ConnectivityPageViewModel.cs
+++ samples/Playground/Playground/ViewModels/Components/ConnectivityPageViewModel.cs
@@ -1,7 +1,8 @@
 // Developed by Softeq Development Corporation
 // http://www.softeq.com
 
-﻿using Plugin.Connectivity.Abstractions;
+using System.Linq;
+using Plugin.Connectivity.Abstractions;
 using Softeq.XToolkit.Connectivity;
 using Softeq.XToolkit.WhiteLabel.Mvvm;
 using Softeq.XToolkit.WhiteLabel.Threading;
@@ -15,17 +16,40 @@ namespace Playground.ViewModels.Components
         public ConnectivityPageViewModel(IConnectivityService connectivityService)
         {
             _connectivityService = connectivityService;
-
-            _connectivityService.ConnectivityChanged += ConnectivityServiceConnectivityChanged;
-            _connectivityService.ConnectivityTypeChanged += ConnectivityServiceConnectivityTypeChanged;
         }
+
+        public string ConnectionStatus => _connectivityService.IsConnected ? "Connected" : "No Connection";
+
+        public string ConnectionTypes => string.Join(", ", _connectivityService.ConnectionTypes);
 
         public override void OnAppearing()
         {
             base.OnAppearing();
 
+            _connectivityService.ConnectivityTypeChanged += (sender, args) =>
+            {
+                var isConnected = args.IsConnected;
+                var connectionTypes = args.ConnectionTypes;
+
+                if (isConnected && connectionTypes.Contains(ConnectionType.WiFi))
+                {
+                    // Wifi connected
+                }
+            };
+
+            _connectivityService.ConnectivityChanged += ConnectivityServiceConnectivityChanged;
+            _connectivityService.ConnectivityTypeChanged += ConnectivityServiceConnectivityTypeChanged;
+
             RaisePropertyChanged(nameof(ConnectionStatus));
             RaisePropertyChanged(nameof(ConnectionTypes));
+        }
+
+        public override void OnDisappearing()
+        {
+            base.OnDisappearing();
+
+            _connectivityService.ConnectivityChanged -= ConnectivityServiceConnectivityChanged;
+            _connectivityService.ConnectivityTypeChanged -= ConnectivityServiceConnectivityTypeChanged;
         }
 
         private void ConnectivityServiceConnectivityChanged(object sender, ConnectivityChangedEventArgs e)
@@ -43,12 +67,5 @@ namespace Playground.ViewModels.Components
                 RaisePropertyChanged(nameof(ConnectionTypes));
             });
         }
-
-        public string ConnectionStatus
-        {
-            get => _connectivityService.IsConnected ? "Connected" : "No Connection";
-        }
-
-        public string ConnectionTypes => string.Join(", ", _connectivityService.ConnectionTypes);
     }
 }
