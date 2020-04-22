ad6ca8be43df32dc91d4378201e8992249ab692d
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-04-22 14:18:56 +0300

    Playground sample: iOS13 Compositional Layout (#300)
    * Add empty page with navigation
    
    * Add new enum extensions method
    
    * Add constraints for enum extensions
    
    * Add ios DummyCell
    
    * Fix build
    
    * Add iOS Compositional Layout sample
    
    * Refactor adaptive sections sample
    
    * Add Nested Groups sample
    
    * Refactor

---
+++ samples/Playground/Playground.iOS/Views/Collections/DummyCell.cs
@@ -0,0 +1,35 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+using System;
+using Foundation;
+using UIKit;
+
+namespace Playground.iOS.Views.Collections
+{
+    public partial class DummyCell : UICollectionViewCell
+    {
+        public static readonly NSString Key = new NSString(nameof(DummyCell));
+        public static readonly UINib Nib;
+
+        static DummyCell() => Nib = UINib.FromName(Key, NSBundle.MainBundle);
+
+        protected DummyCell(IntPtr handle)
+            : base(handle)
+        {
+        }
+
+        public void Configure(string text)
+        {
+            TitleLabel.Text = text;
+        }
+
+        public override void AwakeFromNib()
+        {
+            base.AwakeFromNib();
+
+            Layer.BorderColor = UIColor.Black.CGColor;
+            Layer.BorderWidth = 1.0f;
+        }
+    }
+}
