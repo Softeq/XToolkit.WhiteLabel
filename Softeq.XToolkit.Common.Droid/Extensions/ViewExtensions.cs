a2f7e4279d7ef1a537467db9ca343229c1bb1a3a
TimofeyBurak <timofey.burak@softeq.com>
2020-05-17 15:31:42 +0300

    Featture/setcommand_on_ui_thread (#312)

--- Softeq.XToolkit.Common.Droid/Extensions/ViewExtensions.cs
+++ Softeq.XToolkit.Common.Droid/Extensions/ViewExtensions.cs
@@ -1,6 +1,8 @@
 ﻿// Developed by Softeq Development Corporation
 // http://www.softeq.com
 
+using System;
+using Android.OS;
 using Android.Views;
 
 namespace Softeq.XToolkit.Common.Droid.Extensions
@@ -17,6 +19,23 @@ namespace Softeq.XToolkit.Common.Droid.Extensions
         public static void RemoveFromParent(this View view)
         {
             ((ViewGroup) view.Parent)?.RemoveView(view);
+        }
+
+        /// <summary>
+        ///     Executes provided action on UI thread.
+        /// </summary>
+        /// <param name="view">Instance of a <see cref="T:Android.Views.View"/>.</param>
+        /// <param name="action">Action to be executed.</param>
+        public static void BeginInvokeOnMainThread(this View view, Action action)
+        {
+            if (Looper.MainLooper.IsCurrentThread)
+            {
+                action.Invoke();
+            }
+            else
+            {
+                view.Post(action);
+            }
         }
     }
 }
