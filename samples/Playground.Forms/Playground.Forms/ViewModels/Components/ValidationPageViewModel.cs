dd293d1e3de0fe359089c4f294b4a95a164a57ed
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-07-28 21:36:30 +0300

    Add Validation sample forms

---
+++ samples/Playground.Forms/Playground.Forms/ViewModels/Components/ValidationPageViewModel.cs
@@ -0,0 +1,61 @@
+// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Softeq.XToolkit.Common.Commands;
+using Softeq.XToolkit.WhiteLabel.Mvvm;
+using Softeq.XToolkit.WhiteLabel.Validation;
+using Softeq.XToolkit.WhiteLabel.Validation.Rules;
+
+namespace Playground.Forms.ViewModels.Components
+{
+    public class ValidationPageViewModel : ViewModelBase
+    {
+        private readonly ValidatableGroup _validationGroup;
+
+        public ValidationPageViewModel()
+        {
+            UserName = new ValidatableObject<string>();
+            UserName.AddRule(new IsNotNullOrEmptyRule("A username is required."));
+            UserName.AddRule(new LengthRule(3, 10, "Username must be 3-10 characters."));
+
+            Email = new ValidatableObject<string>();
+            Email.AddRule(new IsNotNullOrEmptyRule("Email is required."));
+            Email.AddRule(new EmailRule("Invalid Email."));
+
+            TermsAndCondition = new ValidatableObject<bool>();
+            TermsAndCondition.AddRule(new IsValueTrueRule("Please accept terms and condition"));
+
+            _validationGroup = new ValidatableGroup(UserName, Email, TermsAndCondition);
+
+            CheckNameCommand = new RelayCommand(CheckName);
+            SubmitCommand = new AsyncCommand(SubmitAsync);
+        }
+
+        public ValidatableObject<string> UserName { get; }
+
+        public ValidatableObject<string> Email { get; }
+
+        public ValidatableObject<bool> TermsAndCondition { get; }
+
+        public ICommand CheckNameCommand { get; }
+
+        public AsyncCommand SubmitCommand { get; }
+
+        private void CheckName()
+        {
+            UserName.Validate();
+        }
+
+        private async Task SubmitAsync()
+        {
+            if (_validationGroup.Validate())
+            {
+                await Task.Delay(3000);
+
+                UserName.Errors.Add("Server validation: Username is not valid");
+            }
+        }
+    }
+}
