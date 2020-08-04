0d8439540bc66823fc17bf36304578f505c69e24
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-08-04 12:44:18 +0300

    Remove unnecessary method GetValueOrDefault

--- Softeq.XToolkit.Common/Extensions/DictionaryExtensions.cs
+++ Softeq.XToolkit.Common/Extensions/DictionaryExtensions.cs
@@ -2,26 +2,11 @@
 // http://www.softeq.com
 
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 
 namespace Softeq.XToolkit.Common.Extensions
 {
     public static class DictionaryExtensions
     {
-        /// <summary>
-        ///     Gets the value for a key. If the key does not exist, return default(TValue).
-        /// </summary>
-        /// <typeparam name="TKey">The type of the keys in the dictionary.</typeparam>
-        /// <typeparam name="TValue">The type of the values in the dictionary.</typeparam>
-        /// <param name="dictionary">The dictionary to call this method on.</param>
-        /// <param name="key">The key to look up.</param>
-        /// <returns>The key value. default(TValue) if this key is not in the dictionary.</returns>
-        [return: MaybeNull]
-        public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
-        {
-            return dictionary.TryGetValue(key, out var result) ? result : default;
-        }
-
         /// <summary>
         ///     Adds value with a specified key to the dictionary.
         ///     If the key already exists in the dictionary, replaces the value for this key.
