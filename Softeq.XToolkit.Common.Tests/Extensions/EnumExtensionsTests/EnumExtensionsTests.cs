ef99aa90f1b732a1688f46ee2e278bd005bd1e67
Alexandra <aleksandra.mashenkova@softeq.com>
2020-08-18 22:18:10 +0300

    Add missing headers

---
+++ Softeq.XToolkit.Common.Tests/Extensions/EnumExtensionsTests/EnumExtensionsTests.cs
@@ -0,0 +1,12 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+using System;
+
+namespace Softeq.XToolkit.Common.Tests.Extensions.EnumExtensionsTests
+{
+    public class EnumExtensionsTests
+    {
+
+    }
+}
