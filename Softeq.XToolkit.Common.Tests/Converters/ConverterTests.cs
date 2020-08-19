5efc52e1c1cff931820cd350a8ee4404e35b3723
vadim.pylsky <vadim.pylsky@softeq.com>
2020-08-19 16:53:55 +0300

    added tests for IConverter

---
+++ Softeq.XToolkit.Common.Tests/Converters/ConverterTests.cs
@@ -0,0 +1,39 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+using System;
+using Xunit;
+
+namespace Softeq.XToolkit.Common.Tests.Converters
+{
+    public class ConverterTests
+    {
+        [Fact]
+        public void ConvertValue()
+        {
+            var converter = ConverterHelper.CreateTwoWayConverter();
+
+            var result = converter.ConvertValue(true);
+
+            Assert.Equal("True", result);
+        }
+
+        [Fact]
+        public void ConvertValueBack()
+        {
+            var converter = ConverterHelper.CreateTwoWayConverter();
+
+            var result = converter.ConvertValueBack("true");
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void ConvertValueBack_ThrowsNotImplementedException()
+        {
+            var converter = ConverterHelper.CreateOneWayConverter();
+
+            Assert.Throws<NotImplementedException>(() => converter.ConvertValueBack("true"));
+        }
+    }
+}
