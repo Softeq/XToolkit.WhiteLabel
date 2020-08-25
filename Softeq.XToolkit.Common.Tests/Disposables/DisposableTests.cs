0d3963303b03a3ded9db78b338310f5b29cf44e7
vadimpylsky <vadim.pylsky@gmail.com>
2020-08-25 13:35:48 +0300

    Add tests for Common.Disposables (#333)

---
+++ Softeq.XToolkit.Common.Tests/Disposables/DisposableTests.cs
@@ -0,0 +1,38 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+using System;
+using Softeq.XToolkit.Common.Disposables;
+using Xunit;
+
+namespace Softeq.XToolkit.Common.Tests.Disposables
+{
+    public class DisposableTests
+    {
+        [Fact]
+        public void Create()
+        {
+            var instance = Disposable.Create(() => { });
+            Assert.True(instance is IDisposable);
+        }
+
+        [Fact]
+        public void Create_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => Disposable.Create(null!));
+        }
+
+        [Fact]
+        public void Dispose()
+        {
+            var disposed = false;
+            var d = Disposable.Create(() => { disposed = true; });
+
+            Assert.False(disposed);
+
+            d.Dispose();
+
+            Assert.True(disposed);
+        }
+    }
+}
