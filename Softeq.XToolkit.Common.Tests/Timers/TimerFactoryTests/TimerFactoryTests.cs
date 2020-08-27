7a350f6d71d5272d7c10f9331ee2a935e1b25d3e
AlexandraMash <aleksandra.mashenkova@softeq.com>
2020-08-27 16:14:26 +0300

    Add tests for Timer and TimerFactory (#338)
    * Add tests for Timer and TimerFactory
    
    * Refactor Timer tests

---
+++ Softeq.XToolkit.Common.Tests/Timers/TimerFactoryTests/TimerFactoryTests.cs
@@ -0,0 +1,35 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+using System;
+using System.Threading.Tasks;
+using NSubstitute;
+using Softeq.XToolkit.Common.Tasks;
+using Softeq.XToolkit.Common.Timers;
+using Xunit;
+
+namespace Softeq.XToolkit.Common.Tests.Timers.TimerFactoryTests
+{
+    public class TimerFactoryTests
+    {
+        private readonly TimerFactory _timerFactory;
+
+        public TimerFactoryTests()
+        {
+            _timerFactory = new TimerFactory();
+        }
+
+        [Fact]
+        public void Create_WithParams_CreatedTimer()
+        {
+            var task = Substitute.For<Func<Task>>();
+            var taskRef = new TaskReference(task);
+            var interval = 1000;
+
+            var result = _timerFactory.Create(taskRef, interval);
+
+            Assert.NotNull(result);
+            Assert.IsType<Timer>(result);
+        }
+    }
+}
