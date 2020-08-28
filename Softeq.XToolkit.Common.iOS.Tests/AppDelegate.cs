42b333079f04b2722d7f160615b98fefcaea6093
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-08-28 17:35:38 +0300

    Cleanup

--- Softeq.XToolkit.Common.iOS.Tests/AppDelegate.cs
+++ Softeq.XToolkit.Common.iOS.Tests/AppDelegate.cs
@@ -9,17 +9,9 @@ using Xunit.Sdk;
 
 namespace Softeq.XToolkit.Common.iOS.Tests
 {
-    // The UIApplicationDelegate for the application. This class is responsible for launching the
-    // User Interface of the application, as well as listening (and optionally responding) to
-    // application events from iOS.
     [Register("AppDelegate")]
     public partial class AppDelegate : RunnerAppDelegate
     {
-        // This method is invoked when the application has loaded and is ready to run. In this
-        // method you should instantiate the window, load the UI into it and then make the window
-        // visible.
-        //
-        // You have 17 seconds to return from this method, or iOS will terminate your application.
         public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
         {
             // We need this to ensure the execution assembly is part of the app bundle
