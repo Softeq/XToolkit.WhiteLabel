8251adc0bff434a66dad66df0f921269297af9ec
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-09-04 14:43:10 +0300

    Add NsLocaleExtensions tests (#353)

---
+++ Softeq.XToolkit.Common.iOS.Tests/Extensions/NsLocaleExtensionsTests/NsLocaleExtensionsTests.cs
@@ -0,0 +1,43 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Foundation;
+using Softeq.XToolkit.Common.iOS.Extensions;
+using Xunit;
+
+namespace Softeq.XToolkit.Common.iOS.Tests.Extensions.NsLocaleExtensionsTests
+{
+    [SuppressMessage("ReSharper", "InvokeAsExtensionMethod", Justification = "Need for tests")]
+    public class NsLocaleExtensionsTests
+    {
+        [Fact]
+        public void Is24HourFormat_Null_ThrowsArgumentNullException()
+        {
+            var locale = null as NSLocale;
+
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                NsLocaleExtensions.Is24HourFormat(locale!);
+            });
+        }
+
+        [Theory]
+        [InlineData("en_US", false)]
+        [InlineData("en_CA", false)]
+        [InlineData("fil_PH", false)]
+        [InlineData("ru_RU", true)]
+        [InlineData("en_BY", true)]
+        [InlineData("de_DE", true)]
+        [InlineData("it_IT", true)]
+        public void Is24HourFormat_24HourLocaleFormat_ReturnsTrue(string localeId, bool expectedResult)
+        {
+            var locale = NSLocale.FromLocaleIdentifier(localeId);
+
+            var result = locale.Is24HourFormat();
+
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
