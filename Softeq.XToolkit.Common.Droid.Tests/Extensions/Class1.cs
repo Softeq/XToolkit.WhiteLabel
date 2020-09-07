f1c3b9ee397a99037ab713b7c2559fa3a9a53e64
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-09-07 22:59:26 +0300

    Add Android tests runner project

---
+++ Softeq.XToolkit.Common.Droid.Tests/Extensions/Class1.cs
@@ -0,0 +1,16 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+using Xunit;
+
+namespace Softeq.XToolkit.Common.Droid.Tests.Extensions
+{
+    public class Class1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
