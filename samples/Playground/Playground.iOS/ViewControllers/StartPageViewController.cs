850052b7a6db7c93358f91a904c1e5421f9d99fd
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-09-08 20:21:42 +0300

    Improve compatibility (#360)

--- samples/Playground/Playground.iOS/ViewControllers/StartPageViewController.cs
+++ samples/Playground/Playground.iOS/ViewControllers/StartPageViewController.cs
@@ -3,11 +3,25 @@
 
 using Playground.ViewModels;
 using Softeq.XToolkit.WhiteLabel.iOS;
+using UIKit;
 
 namespace Playground.iOS.ViewControllers
 {
     public class StartPageViewController : ViewControllerBase<StartPageViewModel>
     {
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            View!.BackgroundColor = GetDefaultColor();
+        }
+
+        private static UIColor GetDefaultColor()
+        {
+            return UIDevice.CurrentDevice.CheckSystemVersion(13, 0)
+                ? UIColor.SystemBackgroundColor
+                : UIColor.White;
+        }
     }
 }
 
