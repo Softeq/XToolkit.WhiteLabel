f7460a6433a5f5f883b7f87fff8b026ce4ae7cd4
savikpavel <pavel.savik@softeq.com>
2020-09-16 14:51:26 +0300

    EditText tests added (Droid)

---
+++ Softeq.XToolkit.Common.Droid.Tests/Extensions/EditTextExtensionsTests/EditTextExtensionsTests.cs
@@ -0,0 +1,95 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+using Android.Text;
+using Android.Widget;
+using Softeq.XToolkit.Common.Droid.Extensions;
+using Softeq.XToolkit.Common.Droid.TextFilters;
+using Xunit;
+
+namespace Softeq.XToolkit.Common.Droid.Tests.Extensions.EditTextExtensionsTests
+{
+    public class EditTextExtensionsTests
+    {
+        [Fact]
+        public void SetFilters_WhenCalledWithoutFilters_DoNothing()
+        {
+            EditText editText = new EditText(MainActivity.Current);
+            editText.SetFilters();
+
+            var appliedFilters = editText.GetFilters();
+            if (appliedFilters != null)
+            {
+                Assert.Empty(appliedFilters);
+            }
+        }
+
+        [Fact]
+        public void SetFilters_WhenCalledWithMultipleFilters_AppliesAllFilters()
+        {
+            EditText editText = new EditText(MainActivity.Current);
+
+            var filter1 = new ForbiddenCharsInputFilter(new char[] { 'a' });
+            var filter2 = new ForbiddenCharsInputFilter(new char[] { 'b' });
+            var filter3 = new ForbiddenCharsInputFilter(new char[] { 'a' });
+            var filters = new IInputFilter[] { filter1, filter2, filter3 };
+
+            editText.SetFilters(filter1, filter2, filter3);
+
+            var appliedFilters = editText.GetFilters();
+            Assert.Equal(filters, appliedFilters);
+        }
+
+        [Fact]
+        public void SetFilters_WhenCalledWithMultipleFiltersWithNullAndDuplicates_AppliesAllFilters()
+        {
+            EditText editText = new EditText(MainActivity.Current);
+
+            var filter1 = new ForbiddenCharsInputFilter(new char[] { 'a' });
+            var filter2 = new ForbiddenCharsInputFilter(new char[] { 'b' });
+            var filter3 = new ForbiddenCharsInputFilter(new char[] { 'a' });
+            var nullFilter = null as IInputFilter;
+            var filters = new IInputFilter[] { filter1, filter2, nullFilter, filter3, filter2, nullFilter, filter1, filter2 };
+
+            editText.SetFilters(filter1, filter2, nullFilter, filter3, filter2, nullFilter, filter1, filter2);
+
+            var appliedFilters = editText.GetFilters();
+            Assert.Equal(filters, appliedFilters);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("a")]
+        [InlineData("abcd")]
+        [InlineData("abcdefg")]
+        public void KeepFocusAtTheEndOfField_WhenTextChangedWithoutFocus_WhenFocused_MovesCursorToTheEndOfField(string str)
+        {
+            EditText editText = new EditText(MainActivity.Current);
+            editText.ClearFocus();
+
+            editText.Text = str;
+            editText.RequestFocus();
+
+            Assert.Equal(editText.SelectionEnd, editText.SelectionStart);
+            Assert.Equal(editText.SelectionStart, str.Length);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("a")]
+        [InlineData("abcd")]
+        [InlineData("abcdefg")]
+        public void KeepFocusAtTheEndOfField_WhenTextChangedWithFocus_WhenRefocused_MovesCursorToTheEndOfField(string str)
+        {
+            EditText editText = new EditText(MainActivity.Current);
+            editText.RequestFocus();
+
+            editText.Text = str;
+            editText.ClearFocus();
+            editText.RequestFocus();
+
+            Assert.Equal(editText.SelectionEnd, editText.SelectionStart);
+            Assert.Equal(editText.SelectionStart, str.Length);
+        }
+    }
+}
---
+++ Softeq.XToolkit.Common.Droid/TextFilters/ForbiddenCharsInputFilter.cs
@@ -0,0 +1,41 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+using System.Linq;
+using Android.Text;
+using Java.Lang;
+
+namespace Softeq.XToolkit.Common.Droid.TextFilters
+{
+    /// <summary>
+    ///     This filter will constrain edits not to make text contains forbidden symbols.
+    /// </summary>
+    public class ForbiddenCharsInputFilter : Object, IInputFilter
+    {
+        private readonly char[] _forbiddenCharacters;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="ForbiddenCharsInputFilter"/> class.
+        /// </summary>
+        /// <param name="forbiddenCharacters">Array of characters not allowed in the input. Ignored when null.</param>
+        public ForbiddenCharsInputFilter(char[] forbiddenCharacters)
+        {
+            _forbiddenCharacters = forbiddenCharacters;
+        }
+
+        /// <inheritdoc />
+        public ICharSequence? FilterFormatted(ICharSequence? source, int start, int end, ISpanned? dest, int dstart, int dend)
+        {
+            if (source != null && _forbiddenCharacters != null)
+            {
+                var intersection = source.ToArray().Intersect(_forbiddenCharacters);
+                if (intersection != null && intersection.Any())
+                {
+                    return new String(string.Empty);
+                }
+            }
+
+            return null;
+        }
+    }
+}
