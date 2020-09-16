5f255097250ad5586151a115ae32da207983a75f
Savikpavel <savikpavel@gmail.com>
2020-09-16 17:10:07 +0300

    Feature/edit text extensions tests (#374)
    * StringExtensionsTests namespace fixed (Droid)
    
    * EditText tests added (Droid)
    
    * EditTextExtensions tests fixed to use Mock filter instead of the real one
    
    * InputFiltermock renamed to MockInputFilter
    
    Co-authored-by: savikpavel <pavel.savik@softeq.com>

--- Softeq.XToolkit.Common.Droid/Extensions/EditTextExtensions.cs
+++ Softeq.XToolkit.Common.Droid/Extensions/EditTextExtensions.cs
@@ -1,10 +1,8 @@
 ﻿// Developed by Softeq Development Corporation
 // http://www.softeq.com
 
-using System.Linq;
 using Android.Text;
 using Android.Widget;
-using Java.Lang;
 
 namespace Softeq.XToolkit.Common.Droid.Extensions
 {
@@ -36,38 +34,6 @@ namespace Softeq.XToolkit.Common.Droid.Extensions
         public static void SetFilters(this EditText editText, params IInputFilter[] filters)
         {
             editText.SetFilters(filters);
-        }
-
-        /// <summary>
-        ///     This filter will constrain edits not to make text contains forbidden symbols.
-        /// </summary>
-        public class ForbiddenCharsInputFilter : Object, IInputFilter
-        {
-            private readonly char[] _forbiddenCharacters;
-
-            /// <summary>
-            ///     Initializes a new instance of the <see cref="ForbiddenCharsInputFilter"/> class.
-            /// </summary>
-            /// <param name="forbiddenCharacters">Array of characters not allowed in the input. Ignored when null.</param>
-            public ForbiddenCharsInputFilter(char[] forbiddenCharacters)
-            {
-                _forbiddenCharacters = forbiddenCharacters;
-            }
-
-            /// <inheritdoc />
-            public ICharSequence? FilterFormatted(ICharSequence? source, int start, int end, ISpanned? dest, int dstart, int dend)
-            {
-                if (source != null && _forbiddenCharacters != null)
-                {
-                    var intersection = source.ToArray().Intersect(_forbiddenCharacters);
-                    if (intersection != null && intersection.Any())
-                    {
-                        return new String(string.Empty);
-                    }
-                }
-
-                return null;
-            }
         }
     }
 }
