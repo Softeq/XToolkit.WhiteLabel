b28ffd7c09abe9340cd79db1029f37fe511e7f86
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-09-16 15:17:11 +0300

    Add DroidMainThreadExecutor tests

---
+++ Softeq.XToolkit.Common.Droid.Tests/DroidMainThreadExecutorTests/DroidMainThreadExecutorTests.cs
@@ -0,0 +1,34 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Softeq.XToolkit.Common.Droid.Tests.DroidMainThreadExecutorTests
+{
+    public class DroidMainThreadExecutorTests
+    {
+        private readonly DroidMainThreadExecutor _executor;
+
+        public DroidMainThreadExecutorTests()
+        {
+            _executor = new DroidMainThreadExecutor();
+        }
+
+        [Fact]
+        public async Task IsMainThread_InMainThread_ReturnsTrue()
+        {
+            var result = await Helpers.RunOnUIThreadAsync(() => _executor.IsMainThread);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public async Task IsMainThread_NotInMainThread_ReturnsFalse()
+        {
+            var result = await Task.Run(() => _executor.IsMainThread);
+
+            Assert.False(result);
+        }
+    }
+}
