ec657bfc66131b5d89be98b696f26532069bb469
Savikpavel <savikpavel@gmail.com>
2020-09-16 13:58:19 +0300

    StringExtensions tests (Droid) (#371)
    * StringExtensions reworked and tests added
    BoolToVisibilityConverter removed
    
    * Language version rollback (according to PR comments)
    
    * Type for GoneConverter Instance fixed
    
    * TextRange used in StringExtensions instead of "startingIndex" and "length"
    
    * ArgumentNullException message simplified (for StringExtensions.Droid)
    
    * Some improves
    
    Co-authored-by: savikpavel <pavel.savik@softeq.com>
    Co-authored-by: Yauheni Pakala <evgeniy.pakalo@gmail.com>

--- Softeq.XToolkit.Common.Droid/Extensions/StringExtensions.cs
+++ Softeq.XToolkit.Common.Droid/Extensions/StringExtensions.cs
@@ -1,8 +1,10 @@
 ﻿// Developed by Softeq Development Corporation
 // http://www.softeq.com
 
+using System;
 using Android.Text;
-using Java.Lang;
+using Softeq.XToolkit.Common.Helpers;
+using Object = Java.Lang.Object;
 
 namespace Softeq.XToolkit.Common.Droid.Extensions
 {
@@ -12,48 +14,88 @@ namespace Softeq.XToolkit.Common.Droid.Extensions
     public static class StringExtensions
     {
         /// <summary>
-        ///     Allows to apply multiple spans to the whole string.
+        ///     Allows to apply multiple spans to part of the string.
         /// </summary>
-        /// <returns>The spannable string with all spans apllied.</returns>
+        /// <returns>The spannable string with all spans applied.</returns>
         /// <param name="unformattedString">Unformatted string.</param>
+        /// <param name="textRange">
+        ///     Instance containing the starting index and the length of the range to which spans shall be applied.
+        /// </param>
         /// <param name="spans">An array of spans.</param>
-        public static SpannableString FormatSpannable(this string unformattedString, params Object[] spans)
+        /// <exception cref="T:System.ArgumentNullException">
+        ///     <paramref name="unformattedString"/> and <paramref name="textRange"/> parameters cannot be <see langword="null"/>.
+        /// </exception>
+        /// <exception cref="T:System.ArgumentException">
+        ///     <paramref name="spans"/> list cannot be empty.
+        /// </exception>
+        /// <exception cref="T:System.ArgumentOutOfRangeException">
+        ///     Starting index of the <paramref name="textRange"/> must be less than the size of the string.
+        ///     - and -
+        ///     Length of the <paramref name="textRange"/> must be less than the size of the string.
+        ///     - and -
+        ///     End index of the <paramref name="textRange"/> must be less than the size of the string.
+        /// </exception>
+        public static SpannableString FormatSpannable(
+            this string unformattedString,
+            TextRange textRange,
+            params Object[] spans)
         {
+            if (unformattedString == null)
+            {
+                throw new ArgumentNullException(nameof(unformattedString));
+            }
+
+            if (textRange == null)
+            {
+                throw new ArgumentNullException(nameof(textRange));
+            }
+
+            if (spans.Length == 0)
+            {
+                throw new ArgumentException($"{nameof(spans)} must be non-empty");
+            }
+
+            if (textRange.Position >= unformattedString.Length)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(textRange.Position)} must be < {unformattedString.Length}");
+            }
+
+            if (textRange.Length > unformattedString.Length)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(textRange.Length)} must be < {unformattedString.Length}");
+            }
+
+            if (textRange.Position + textRange.Length > unformattedString.Length)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(textRange.Position)} + {nameof(textRange.Length)} must be <= {unformattedString.Length}");
+            }
+
             var formattedString = new SpannableString(unformattedString);
             foreach (var span in spans)
             {
-                formattedString.SetSpan(span, 0, unformattedString.Length, SpanTypes.ExclusiveExclusive);
+                formattedString.SetSpan(span, textRange.Position, textRange.Position + textRange.Length, SpanTypes.ExclusiveExclusive);
             }
 
             return formattedString;
         }
 
         /// <summary>
-        ///     Allows to apply multiple spans to part of the string.
+        ///     Allows to apply multiple spans to the whole string.
         /// </summary>
-        /// <returns>The spannable string with all spans apllied.</returns>
+        /// <returns>The spannable string with all spans applied.</returns>
         /// <param name="unformattedString">Unformatted string.</param>
-        /// <param name="startingIndex">Starting index from which spans shall be applied.</param>
-        /// <param name="length">Length of range to which spans shall be applied.</param>
         /// <param name="spans">An array of spans.</param>
-        public static SpannableString FormatSpannable(
-            this string unformattedString,
-            int startingIndex,
-            int length,
-            params Object[] spans)
+        /// <exception cref="T:System.ArgumentNullException">
+        ///     <paramref name="unformattedString"/> parameter cannot be <see langword="null"/>.
+        /// </exception>
+        public static SpannableString FormatSpannable(this string unformattedString, params Object[] spans)
         {
-            startingIndex = startingIndex < 0 ? 0 : startingIndex;
-            length = startingIndex + length >= unformattedString.Length
-                ? unformattedString.Length - startingIndex - 1
-                : length;
-
-            var formattedString = new SpannableString(unformattedString);
-            foreach (var span in spans)
+            if (unformattedString == null)
             {
-                formattedString.SetSpan(span, startingIndex, length, SpanTypes.ExclusiveExclusive);
+                throw new ArgumentNullException(nameof(unformattedString));
             }
 
-            return formattedString;
+            return unformattedString.FormatSpannable(new TextRange(0, unformattedString.Length), spans);
         }
     }
 }
