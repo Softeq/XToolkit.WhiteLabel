3aee714be37f70580d431db2c07fcc7b7d336422
savikpavel <pavel.savik@softeq.com>
2020-09-18 16:08:33 +0300

    Validation rules tests reworked a little bit Documentation for validation rules added where needed

---
+++ Softeq.XToolkit.WhiteLabel.Tests/Validation/Rules/IsValueTrueRuleTests.cs
@@ -0,0 +1,46 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+using Softeq.XToolkit.WhiteLabel.Validation;
+using Softeq.XToolkit.WhiteLabel.Validation.Rules;
+using Xunit;
+
+namespace Softeq.XToolkit.WhiteLabel.Tests.Validation.Rules
+{
+    public class IsValueTrueRuleTests
+    {
+        private const string ErrorMessage = "error message";
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(ErrorMessage)]
+        public void Ctor_WithValidValues_InitializesPropertiesCorrectly(string message)
+        {
+            var rule = new IsValueTrueRule(message);
+
+            Assert.Equal(message, rule.ValidationMessage);
+            Assert.IsAssignableFrom<IValidationRule<bool>>(rule);
+        }
+
+        [Fact]
+        public void Check_ForFalseValue_ReturnsFalse()
+        {
+            var rule = new IsValueTrueRule(ErrorMessage);
+
+            var isValid = rule.Check(false);
+
+            Assert.False(isValid);
+        }
+
+        [Fact]
+        public void Check_ForTrueValue_ReturnsTrue()
+        {
+            var rule = new IsValueTrueRule(ErrorMessage);
+
+            var isValid = rule.Check(true);
+
+            Assert.True(isValid);
+        }
+    }
+}
