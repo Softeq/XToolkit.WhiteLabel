5a032ae79fe62d76910bfaff1dfc14a58ff05182
savikpavel <pavel.savik@softeq.com>
2020-09-22 15:24:07 +0300

    AvatarPlaceholderBuilder methods reworked and tests and documentation added

--- Softeq.XToolkit.WhiteLabel/Helpers/AvatarPlaceholderBuilder.cs
+++ Softeq.XToolkit.WhiteLabel/Helpers/AvatarPlaceholderBuilder.cs
@@ -8,11 +8,75 @@ namespace Softeq.XToolkit.WhiteLabel.Helpers
 {
     public static class AvatarPlaceholderBuilder
     {
-        public static (string Text, string Color) Build(string name, string[]? colors = null)
+        /// <summary>
+        ///     Creates an abbreviation from the specified username
+        ///     and maps it to the color from the predefined list.
+        ///     <para/>
+        ///     For more info see:
+        ///     <para/>
+        ///     <see cref="M:Softeq.XToolkit.WhiteLabel.Helpers.AvatarPlaceholderBuilder.GetColor(System.String)"/>.
+        ///     <para/>
+        ///     <see cref="M:Softeq.XToolkit.WhiteLabel.Helpers.AvatarPlaceholderBuilder.GetAbbreviation(System.String)"/>.
+        /// </summary>
+        /// <param name="userName">String containing the username to create abbreviation from.</param>
+        /// <returns>
+        ///     Tuple containing the created abbreviation and picked color.
+        /// </returns>
+        /// <exception cref="T:System.ArgumentNullException">
+        ///     <paramref name="userName"/> cannot be <see langword="null"/>.
+        /// </exception>
+        public static (string Text, string Color) GetAbbreviationAndColor(string userName)
         {
-            if (colors == null)
-            {
-                colors = new[]
+            var abbr = GetAbbreviation(userName);
+            var color = GetColor(abbr);
+
+            return (abbr, color);
+        }
+
+        /// <summary>
+        ///     Creates an abbreviation from the specified username
+        ///     and maps it to the color from the specified list.
+        ///     <para/>
+        ///     For more info see:
+        ///     <para/>
+        ///     <see cref="M:Softeq.XToolkit.WhiteLabel.Helpers.AvatarPlaceholderBuilder.GetColor(System.String,System.String[])"/>.
+        ///     <para/>
+        ///     <see cref="M:Softeq.XToolkit.WhiteLabel.Helpers.AvatarPlaceholderBuilder.GetAbbreviation(System.String)"/>.
+        /// </summary>
+        /// <param name="userName">String containing the username to create abbreviation from.</param>
+        /// <param name="colors">List of colors (normally in HEX format) to use.</param>
+        /// <returns>
+        ///     Tuple containing the created abbreviation and picked color.
+        /// </returns>
+        /// <exception cref="T:System.ArgumentNullException">
+        ///     <paramref name="userName"/> and <paramref name="colors"/> cannot be <see langword="null"/>.
+        /// </exception>
+        /// <exception cref="T:System.ArgumentException">
+        ///     <paramref name="colors"/> cannot be empty.
+        /// </exception>
+        public static (string Text, string Color) GetAbbreviationAndColor(string userName, string[] colors)
+        {
+            var abbr = GetAbbreviation(userName);
+            var color = GetColor(abbr, colors);
+
+            return (abbr, color);
+        }
+
+        /// <summary>
+        ///     Maps the specified username to the color from the predefined list.
+        ///     The mapping relationship used in this method is many-to-one,
+        ///     so that one color can be mapped to many different usernames.
+        /// </summary>
+        /// <param name="userName">String containing the username to pick color for.</param>
+        /// <returns>
+        ///     String containing the picked color.
+        /// </returns>
+        /// <exception cref="T:System.ArgumentNullException">
+        ///     <paramref name="userName"/> cannot be <see langword="null"/>.
+        /// </exception>
+        public static string GetColor(string userName)
+        {
+            var colors = new[]
                 {
                     "#1abc9c",
                     "#2ecc71",
@@ -34,22 +98,71 @@ namespace Softeq.XToolkit.WhiteLabel.Helpers
                     "#bdc3c7",
                     "#7f8c8d"
                 };
-            }
-
-            var abbr = GetAbbreviation(name);
-            var index = Math.Abs(abbr.GetHashCode()) % Math.Max(1, colors.Length - 1);
-
-            return (abbr, colors[index]);
+            return GetColor(userName, colors);
         }
 
-        private static string GetAbbreviation(string data)
+        /// <summary>
+        ///     Maps the specified username to the color from the specified list.
+        ///     The mapping relationship used in this method is many-to-one,
+        ///     so that one color can be mapped to many different usernames.
+        /// </summary>
+        /// <param name="userName">String containing the username to pick color for.</param>
+        /// <param name="colors">List of colors (normally in HEX format) to use.</param>
+        /// <returns>
+        ///     String containing the picked color.
+        /// </returns>
+        /// <exception cref="T:System.ArgumentNullException">
+        ///     <paramref name="userName"/> and <paramref name="colors"/> cannot be <see langword="null"/>.
+        /// </exception>
+        /// <exception cref="T:System.ArgumentException">
+        ///     <paramref name="colors"/> cannot be empty.
+        /// </exception>
+        public static string GetColor(string userName, string[] colors)
         {
-            if (string.IsNullOrWhiteSpace(data))
+            if (userName == null)
+            {
+                throw new ArgumentNullException(nameof(userName));
+            }
+
+            if (colors == null)
+            {
+                throw new ArgumentNullException(nameof(colors));
+            }
+
+            if (colors.Length == 0)
+            {
+                throw new ArgumentException("Colors array cannot be empty", nameof(colors));
+            }
+
+            var index = Math.Abs(userName.GetHashCode()) % Math.Max(1, colors.Length - 1);
+            return colors[index];
+        }
+
+        /// <summary>
+        ///     Creates an abbreviation from the specified username.
+        ///     Abbreviation will contain no more than 3 uppercase letters.
+        /// </summary>
+        /// <param name="userName">String containing the username to create abbreviation from.</param>
+        /// <returns>
+        ///     String containing the abbreviation
+        ///     or empty string if the <paramref name="userName"/> string is empty or contains only whitespace characters.
+        /// </returns>
+        /// <exception cref="T:System.ArgumentNullException">
+        ///     <paramref name="userName"/> cannot be <see langword="null"/>.
+        /// </exception>
+        public static string GetAbbreviation(string userName)
+        {
+            if (userName == null)
+            {
+                throw new ArgumentNullException(nameof(userName));
+            }
+
+            if (string.IsNullOrWhiteSpace(userName))
             {
                 return string.Empty;
             }
 
-            var trimmedData = data.Trim();
+            var trimmedData = userName.Trim();
 
             // case for: First Last -> FL
             if (trimmedData.Contains(' '))
