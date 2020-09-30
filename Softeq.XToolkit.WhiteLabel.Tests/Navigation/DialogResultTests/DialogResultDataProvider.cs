2924888aab128061225a9b9e6d214816aa2fbe2d
savikpavel <pavel.savik@softeq.com>
2020-09-30 16:23:08 +0300

    Documentation and tests added for NavigationParameterModel and DialogResult

---
+++ Softeq.XToolkit.WhiteLabel.Tests/Navigation/DialogResultTests/DialogResultDataProvider.cs
@@ -0,0 +1,29 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Softeq.XToolkit.WhiteLabel.Tests.Navigation.DialogResultTests
+{
+    internal static class DialogResultDataProvider
+    {
+        private static readonly Task<bool> CompletedTask = Task.FromResult(true);
+
+        public static TheoryData<Task<bool>> CtorData
+           => new TheoryData<Task<bool>>
+           {
+               { null },
+               { CompletedTask },
+           };
+
+        public static TheoryData<string, Task<bool>> GenericCtorData
+           => new TheoryData<string, Task<bool>>
+           {
+               { null, null },
+               { null, CompletedTask },
+               { string.Empty, null },
+               { string.Empty, CompletedTask },
+           };
+    }
+}
--- Softeq.XToolkit.WhiteLabel/Navigation/NavigationParameterModel.cs
+++ Softeq.XToolkit.WhiteLabel/Navigation/NavigationParameterModel.cs
@@ -3,16 +3,34 @@
 
 namespace Softeq.XToolkit.WhiteLabel.Navigation
 {
+    /// <summary>
+    ///     Model for transferring information while navigating from one screento another.
+    /// </summary>
     public class NavigationParameterModel
     {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="NavigationParameterModel"/> class.
+        /// </summary>
+        /// <param name="value">Parameter value.</param>
+        /// <param name="propertyInfo">
+        ///     Model representing information about
+        ///     a property that should store <paramref name="value"/>.
+        /// </param>
         public NavigationParameterModel(object? value, PropertyInfoModel propertyInfo)
         {
             Value = value;
             PropertyInfo = propertyInfo;
         }
 
+        /// <summary>
+        ///     Gets the value of the parameter.
+        /// </summary>
         public object? Value { get; }
 
+        /// <summary>
+        ///     Gets the model representing information about
+        ///     a property that should store <paramref name="value"/>.
+        /// </summary>
         public PropertyInfoModel PropertyInfo { get; }
     }
 }
