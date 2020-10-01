50c0f22de6be091d92425ed7aa15d76efb8b9ab1
savikpavel <pavel.savik@softeq.com>
2020-10-01 21:09:12 +0300

    Tests and documentation for CommandAction

---
+++ Softeq.XToolkit.WhiteLabel.Tests/Mvvm/CommandActionTests/CommandActionTestsDataProvider.cs
@@ -0,0 +1,24 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+using System.Windows.Input;
+using NSubstitute;
+using Softeq.XToolkit.WhiteLabel.Mvvm;
+using Xunit;
+
+namespace Softeq.XToolkit.WhiteLabel.Tests.Mvvm.CommandActionTests
+{
+    internal static class CommandActionTestsDataProvider
+    {
+        private static readonly ICommand CommandSubstitute = Substitute.For<ICommand>();
+
+        public static TheoryData<ICommand, string, CommandActionStyle> CtorData
+           => new TheoryData<ICommand, string, CommandActionStyle>
+           {
+               { null, null, CommandActionStyle.Default },
+               { null, string.Empty, CommandActionStyle.Destructive },
+               { CommandSubstitute, null, CommandActionStyle.Cancel },
+               { CommandSubstitute, string.Empty, CommandActionStyle.Default },
+           };
+    }
+}
