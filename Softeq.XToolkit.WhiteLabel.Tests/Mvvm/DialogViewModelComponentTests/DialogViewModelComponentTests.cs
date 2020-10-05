aef374039806b38483e31b1f4c32a0287a0572e4
Savikpavel <savikpavel@gmail.com>
2020-10-05 22:04:03 +0300

    Tests and documentation for DialogViewModelComponent (#391)
    * Tests and documentation for DialogViewModelComponent
    
    * Redundant full name removed from documentation
    
    Co-authored-by: savikpavel <pavel.savik@softeq.com>

---
+++ Softeq.XToolkit.WhiteLabel.Tests/Mvvm/DialogViewModelComponentTests/DialogViewModelComponentTests.cs
@@ -0,0 +1,37 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+using System;
+using Softeq.XToolkit.WhiteLabel.Mvvm;
+using Xunit;
+
+namespace Softeq.XToolkit.WhiteLabel.Tests.Mvvm.DialogViewModelComponentTests
+{
+    public class DialogViewModelComponentTests
+    {
+        [Fact]
+        public void Ctor_InitializesProperties()
+        {
+            var dialogViewModelComponent = new DialogViewModelComponent();
+
+            Assert.NotNull(dialogViewModelComponent.CloseCommand);
+            Assert.NotNull(dialogViewModelComponent.Task);
+        }
+
+        [Fact]
+        public void CloseCommand_CompletesTaskWithCorrectResultAndNotifies()
+        {
+            var param = new object();
+            var dialogViewModelComponent = new DialogViewModelComponent();
+
+            var raisedEvent = Assert.Raises<EventArgs>(
+                    handler => dialogViewModelComponent.Closed += (s, e) => handler.Invoke(s, e),
+                    handler => { },
+                    () => dialogViewModelComponent.CloseCommand.Execute(param));
+
+            Assert.NotNull(raisedEvent);
+            Assert.True(dialogViewModelComponent.Task.IsCompleted);
+            Assert.Equal(param, dialogViewModelComponent.Task.Result);
+        }
+    }
+}
--- Softeq.XToolkit.WhiteLabel/Mvvm/DialogViewModelBase.cs
+++ Softeq.XToolkit.WhiteLabel/Mvvm/DialogViewModelBase.cs
@@ -5,6 +5,9 @@ using Softeq.XToolkit.WhiteLabel.Navigation;
 
 namespace Softeq.XToolkit.WhiteLabel.Mvvm
 {
+    /// <summary>
+    ///     ViewModel for the modal dialogs.
+    /// </summary>
     public class DialogViewModelBase : ViewModelBase, IDialogViewModel
     {
         protected DialogViewModelBase()
@@ -12,6 +15,11 @@ namespace Softeq.XToolkit.WhiteLabel.Mvvm
             DialogComponent = new DialogViewModelComponent();
         }
 
+        /// <summary>
+        ///     Gets the instance of <see cref="DialogViewModelComponent"/>
+        ///     that contains all the required methods and properties
+        ///     to close the dialog and track its state.
+        /// </summary>
         public DialogViewModelComponent DialogComponent { get; }
     }
 }
