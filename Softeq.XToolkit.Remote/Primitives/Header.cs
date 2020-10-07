10a11238ad316beee8a87882548c66cbbd3b5e77
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-10-07 20:44:37 +0300

    Refactor Remote.Primitives

--- Softeq.XToolkit.Remote/Primitives/Header.cs
+++ Softeq.XToolkit.Remote/Primitives/Header.cs
@@ -3,10 +3,24 @@
 
 namespace Softeq.XToolkit.Remote.Primitives
 {
+    /// <summary>
+    ///    Common headers for Refit interfaces.
+    /// </summary>
     public static class Header
     {
+        /// <summary>
+        ///     The "Accept-Encoding" header advertises that the client can understand Gzip compression.
+        /// </summary>
         public const string Gzip = "Accept-Encoding: gzip";
+
+        /// <summary>
+        ///     The "Accept" header advertises that the client can understand JSON format.
+        /// </summary>
         public const string Json = "Accept: application/json";
+
+        /// <summary>
+        ///     The "Authentication" header with Bearer type.
+        /// </summary>
         public const string BearerAuth = "Authorization: Bearer";
     }
 }
