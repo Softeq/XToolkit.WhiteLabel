6c4cb0b511d6e1dfd4350247145f221c3c8decc9
savikpavel <pavel.savik@softeq.com>
2020-10-13 15:24:12 +0300

    Tests and documentation for DialogsServiceExtensions and FrameNavigationServiceExtensions

---
+++ Softeq.XToolkit.WhiteLabel.Tests/Navigation/DialogsServiceExtensionsTests/DialogsServiceExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+using System;
+using NSubstitute;
+using Softeq.XToolkit.WhiteLabel.Navigation;
+using Softeq.XToolkit.WhiteLabel.Tests.Stubs;
+using Xunit;
+
+namespace Softeq.XToolkit.WhiteLabel.Tests.Navigation.DialogsServiceExtensionsTests
+{
+    public class DialogsServiceExtensionsTests
+    {
+        [Fact]
+        public void For_WhenCalledOnNull_ThrowsCorrectException()
+        {
+            IDialogsService dialogsService = null;
+
+            Assert.Throws<ArgumentNullException>(() => dialogsService.For<DialogViewModelStub>());
+        }
+
+        [Fact]
+        public void For_WhenCalledOnNonNull_CreatesFluentNavigator()
+        {
+            var dialogsService = Substitute.For<IDialogsService>();
+
+            var fluentNavigator = dialogsService.For<DialogViewModelStub>();
+
+            Assert.NotNull(fluentNavigator);
+        }
+    }
+}
--- Softeq.XToolkit.WhiteLabel/Navigation/DialogsServiceExtensions.cs
+++ Softeq.XToolkit.WhiteLabel/Navigation/DialogsServiceExtensions.cs
@@ -1,16 +1,34 @@
 ﻿// Developed by Softeq Development Corporation
 // http://www.softeq.com
 
-using Softeq.XToolkit.WhiteLabel.Mvvm;
+using System;
 using Softeq.XToolkit.WhiteLabel.Navigation.FluentNavigators;
 
 namespace Softeq.XToolkit.WhiteLabel.Navigation
 {
     public static class DialogsServiceExtensions
     {
+        /// <summary>
+        ///     Creates a <see cref="DialogFluentNavigator{TViewModel}"/>
+        ///     from the specified <see cref="IDialogsService"/>.
+        /// </summary>
+        /// <typeparam name="T">Type of ViewModel.</typeparam>
+        /// <param name="dialogsService">Instance of <see cref="IDialogsService"/>.</param>
+        /// <returns>
+        ///     Instance of <see cref="DialogFluentNavigator{TViewModel}"/>
+        ///     created from the specified <see cref="IDialogsService"/>.
+        /// </returns>
+        /// <exception cref="T:System.ArgumentNullException">
+        ///     <paramref name="dialogsService"/> cannot be <see langword="null"/>.
+        /// </exception>
         public static DialogFluentNavigator<T> For<T>(this IDialogsService dialogsService)
-            where T : DialogViewModelBase
+            where T : IDialogViewModel
         {
+            if (dialogsService == null)
+            {
+                throw new ArgumentNullException(nameof(dialogsService));
+            }
+
             return new DialogFluentNavigator<T>(dialogsService);
         }
     }
