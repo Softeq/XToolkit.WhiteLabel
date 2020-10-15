6552223bb322fc42cb2b1061b2e0c05e6cbf6e58
Savikpavel <savikpavel@gmail.com>
2020-10-15 17:30:06 +0300

    DialogConfigs and DryIocContainerAdapter tests (#399)
    * Tests and documentation added for dialog configs;
    Tests and documentation added for DryIocContainerAdapter;
    Documentation for BootstrapperBase and ContainerBuilder reworked
    
    * Build fix
    
    Co-authored-by: savikpavel <pavel.savik@softeq.com>

---
+++ Softeq.XToolkit.WhiteLabel.Tests/Bootstrapper/Containers/DryIocContainerBuilderTests/DryIocContainerBuilderTests.cs
@@ -0,0 +1,14 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+using System;
+
+namespace Softeq.XToolkit.WhiteLabel.Tests.Bootstrapper.Containers.DryIocContainerBuilderTests
+{
+    public class DryIocContainerBuilderTests
+    {
+        public DryIocContainerBuilderTests()
+        {
+        }
+    }
+}
--- Softeq.XToolkit.WhiteLabel.Tests/Stubs/TypeStubs.cs
+++ Softeq.XToolkit.WhiteLabel.Tests/Stubs/TypeStubs.cs
@@ -3,7 +3,7 @@
 
 using Softeq.XToolkit.WhiteLabel.Mvvm;
 
-namespace Softeq.XToolkit.Whitelabel.Tests.Bootstrapper.DefaultViewModelFinderTests
+namespace Softeq.XToolkit.WhiteLabel.Tests.Stubs
 {
     internal interface IDroidViewStub { }
 
