78d9d953eb37d60f52b012a6d8cd23ce6f8270e3
Savikpavel <savikpavel@gmail.com>
2020-10-20 16:03:18 +0300

    Tests and documentation for BackStackManager added (#400)
    Co-authored-by: savikpavel <pavel.savik@softeq.com>

---
+++ Softeq.XToolkit.WhiteLabel.Tests/Navigation/BackStackManagerTests/BackStackManagerTestsDataProvider.cs
@@ -0,0 +1,61 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+using System.Collections.Generic;
+using Softeq.XToolkit.WhiteLabel.Mvvm;
+using Softeq.XToolkit.WhiteLabel.Tests.Stubs;
+using Xunit;
+
+namespace Softeq.XToolkit.WhiteLabel.Tests.Navigation.BackStackManagerTests
+{
+    internal static class BackStackManagerTestsDataProvider
+    {
+        private static readonly IViewModelBase _viewModel1 = new ViewModelStub();
+        private static readonly IViewModelBase _viewModel2 = new ViewModelStub();
+        private static readonly IViewModelBase _viewModel3 = new ViewModelStub();
+
+        public static TheoryData<List<IViewModelBase>> ClearTestData
+           => new TheoryData<List<IViewModelBase>>
+           {
+               { new List<IViewModelBase> { } },
+               { new List<IViewModelBase> { null } },
+               { new List<IViewModelBase> { _viewModel1 } },
+               { new List<IViewModelBase> { _viewModel1, null } },
+               { new List<IViewModelBase> { _viewModel1, _viewModel2, null } },
+               { new List<IViewModelBase> { null, _viewModel1 } },
+               { new List<IViewModelBase> { null, _viewModel1, _viewModel2 } },
+               { new List<IViewModelBase> { _viewModel1, null, _viewModel3 } },
+               { new List<IViewModelBase> { _viewModel1, _viewModel2, _viewModel3 } },
+           };
+
+        public static TheoryData<List<IViewModelBase>, IViewModelBase> PopAndPeekViewModelTestsData
+           => new TheoryData<List<IViewModelBase>, IViewModelBase>
+           {
+               { new List<IViewModelBase> { null }, null },
+               { new List<IViewModelBase> { _viewModel1 }, _viewModel1 },
+               { new List<IViewModelBase> { _viewModel1, null }, null },
+               { new List<IViewModelBase> { _viewModel1, _viewModel2, null }, null },
+               { new List<IViewModelBase> { null, _viewModel1 }, _viewModel1 },
+               { new List<IViewModelBase> { null, _viewModel1, _viewModel2 }, _viewModel2 },
+               { new List<IViewModelBase> { _viewModel1, null, _viewModel3 }, _viewModel3 },
+               { new List<IViewModelBase> { _viewModel1, _viewModel2, _viewModel3 }, _viewModel3 },
+           };
+
+        public static TheoryData<List<IViewModelBase>, IViewModelBase> PushViewModelTestData
+           => new TheoryData<List<IViewModelBase>, IViewModelBase>
+           {
+               { new List<IViewModelBase> { }, null },
+               { new List<IViewModelBase> { }, _viewModel1 },
+               { new List<IViewModelBase> { null }, null },
+               { new List<IViewModelBase> { null }, _viewModel1 },
+               { new List<IViewModelBase> { _viewModel1 }, null },
+               { new List<IViewModelBase> { _viewModel1 }, _viewModel1 },
+               { new List<IViewModelBase> { _viewModel2 }, _viewModel1 },
+               { new List<IViewModelBase> { _viewModel1, _viewModel2, null }, _viewModel3 },
+               { new List<IViewModelBase> { null, _viewModel1 }, _viewModel1 },
+               { new List<IViewModelBase> { null, _viewModel1, _viewModel2 }, null },
+               { new List<IViewModelBase> { _viewModel1, null, _viewModel3 }, _viewModel2 },
+               { new List<IViewModelBase> { _viewModel1, _viewModel2, _viewModel3 }, _viewModel2 },
+           };
+    }
+}
--- Softeq.XToolkit.WhiteLabel/Dependencies.cs
+++ Softeq.XToolkit.WhiteLabel/Dependencies.cs
@@ -10,26 +10,53 @@ using Softeq.XToolkit.WhiteLabel.Navigation;
 namespace Softeq.XToolkit.WhiteLabel
 {
     /// <summary>
-    ///     Please, avoid extending this class.
-    ///     Use it only if you don't have another options to add reference on service.
+    ///     Static class that contains referencies to application services.
+    ///     <para/>
+    ///     Please, avoid using this class.
+    ///     Use it only if you don't have other options to add service reference.
     /// </summary>
     public static class Dependencies
     {
+        /// <summary>
+        ///     Gets a value indicating whether the <see cref="Dependencies"/> service has been initialized.
+        /// </summary>
         public static bool IsInitialized { get; private set; }
 
+        /// <summary>
+        ///     Gets the <see cref="IContainer"/> instance that will be used for resolving dependencies.
+        /// </summary>
         public static IContainer Container { get; private set; } = default!;
 
-        public static IPageNavigationService PageNavigationService => Container.Resolve<IPageNavigationService>();
+        public static IPageNavigationService PageNavigationService => Resolve<IPageNavigationService>();
 
-        public static IJsonSerializer JsonSerializer => Container.Resolve<IJsonSerializer>();
+        public static IJsonSerializer JsonSerializer => Resolve<IJsonSerializer>();
 
-        public static IFileProvider InternalStorageProvider => Container.Resolve<InternalStorageFileProvider>();
+        public static IFileProvider InternalStorageProvider => Resolve<InternalStorageFileProvider>();
 
+        private static T Resolve<T>() where T : notnull
+        {
+            if (!IsInitialized)
+            {
+                throw new InvalidOperationException("Dependencies service is not initialized!");
+            }
+
+            return Container.Resolve<T>();
+        }
+
+        /// <summary>
+        ///     Initializes <see cref="Dependencies"/> service with the specified <see cref="IContainer"/>.
+        /// </summary>
+        /// <param name="iocContainer">IoC container that will be used for resolving dependencies.</param>
         public static void Initialize(IContainer iocContainer)
         {
             if (IsInitialized)
             {
-                throw new InvalidOperationException($"{nameof(Dependencies)} already initialized");
+                throw new InvalidOperationException($"{nameof(Dependencies)} class is already initialized");
+            }
+
+            if (iocContainer == null)
+            {
+                throw new ArgumentNullException(nameof(iocContainer));
             }
 
             Container = iocContainer;
