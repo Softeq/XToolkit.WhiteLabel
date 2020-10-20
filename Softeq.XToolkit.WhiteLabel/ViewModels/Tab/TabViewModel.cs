5129afa0a4ee1fb6731ef61b65fd4d9c0b3c2a81
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-10-20 15:22:45 +0300

    Refactor TabItem to use ctor instead of Initialize & add doc

--- Softeq.XToolkit.WhiteLabel/ViewModels/Tab/TabViewModel.cs
+++ Softeq.XToolkit.WhiteLabel/ViewModels/Tab/TabViewModel.cs
@@ -8,75 +8,72 @@ using Softeq.XToolkit.WhiteLabel.Navigation;
 
 namespace Softeq.XToolkit.WhiteLabel.ViewModels.Tab
 {
+    /// <summary>
+    ///     Base view model to describe the root tab.
+    /// </summary>
+    /// <typeparam name="TKey">The type of the tab key.</typeparam>
     public abstract class TabViewModel<TKey> : RootFrameNavigationViewModelBase
     {
+        private readonly TabItem<TKey> _tab;
+
         private string? _badgeText;
         private bool _isBadgeVisible;
-        private TabItem<TKey> _tab = default!;
 
-        protected TabViewModel(IFrameNavigationService frameNavigationService)
+        protected TabViewModel(
+            IFrameNavigationService frameNavigationService,
+            TabItem<TKey> tab)
             : base(frameNavigationService)
         {
+            _tab = tab ?? throw new ArgumentNullException(nameof(tab));
         }
 
+        /// <summary>
+        ///     Gets or sets the text for the tab badge.
+        /// </summary>
         public string? BadgeText
         {
             get => _badgeText;
             set => Set(ref _badgeText, value);
         }
 
+        /// <summary>
+        ///     Gets or sets a value indicating whether the tab badge visible.
+        /// </summary>
         public bool IsBadgeVisible
         {
             get => _isBadgeVisible;
             set => Set(ref _isBadgeVisible, value);
         }
 
-        public string Title
-        {
-            get
-            {
-                EnsureInitialization();
-                return _tab.Title;
-            }
-        }
+        /// <inheritdoc cref="TabItem{TKey}.Title" />
+        public string Title => _tab.Title;
 
-        public TKey Key
-        {
-            get
-            {
-                EnsureInitialization();
-                return _tab.Key;
-            }
-        }
+        /// <inheritdoc cref="TabItem{TKey}.Key" />
+        public TKey Key => _tab.Key;
 
+        /// <inheritdoc cref="IFrameNavigationService.CanGoBack"/>
         public bool CanGoBack => FrameNavigationService.CanGoBack;
 
-        public void GoBack()
-        {
-            FrameNavigationService.GoBack();
-        }
-
-        public void Initialize(TabItem<TKey> tab)
-        {
-            _tab = tab;
-        }
-
-        private void EnsureInitialization()
-        {
-            if (_tab == null)
-            {
-                throw new InvalidOperationException("ViewModel is not initialized");
-            }
-        }
+        /// <inheritdoc cref="IFrameNavigationService.GoBack()"/>
+        public void GoBack() => FrameNavigationService.GoBack();
     }
 
-    public class TabViewModel<TFirstViewModel, TKey> : TabViewModel<TKey> where TFirstViewModel : IViewModelBase
+    /// <summary>
+    ///     The view model to describe the root tab with navigation to the first page.
+    /// </summary>
+    /// <typeparam name="TFirstViewModel">The type of first view model in the navigation stack.</typeparam>
+    /// <typeparam name="TKey">The type of the tab key.</typeparam>
+    public class TabViewModel<TFirstViewModel, TKey>
+        : TabViewModel<TKey> where TFirstViewModel : IViewModelBase
     {
-        public TabViewModel(IFrameNavigationService frameNavigationService)
-            : base(frameNavigationService)
+        public TabViewModel(
+            IFrameNavigationService frameNavigationService,
+            TabItem<TKey> tab)
+            : base(frameNavigationService, tab)
         {
         }
 
+        /// <inheritdoc cref="IFrameNavigationService.NavigateToFirstPage()"/>
         public override void NavigateToFirstPage()
         {
             // Check fast-backward nav by tab selected
