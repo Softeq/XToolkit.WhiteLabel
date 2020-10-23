e6e08751d23d9f8641e833a965592bcf12eb0b41
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-10-23 22:25:07 +0300

    Add basic PaginationSearchViewModelBase tests

---
+++ Softeq.XToolkit.WhiteLabel.Tests/ViewModels/PaginationSearchViewModelBaseTests/PaginationSearchViewModelBaseTests.cs
@@ -0,0 +1,65 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Softeq.XToolkit.WhiteLabel.Model;
+using Softeq.XToolkit.WhiteLabel.ViewModels;
+using Xunit;
+
+namespace Softeq.XToolkit.WhiteLabel.Tests.ViewModels.PaginationSearchViewModelBaseTests
+{
+    public class PaginationSearchViewModelBaseTests
+    {
+        private readonly TestPaginationSearchViewModelBaseTests _viewModel;
+
+        public PaginationSearchViewModelBaseTests()
+        {
+            _viewModel = new TestPaginationSearchViewModelBaseTests();
+        }
+
+        [Fact]
+        public void ClearCommand_Default_ReturnsICommand()
+        {
+            Assert.IsAssignableFrom<ICommand>(_viewModel.ClearCommand);
+        }
+
+        [Fact]
+        public void SearchCommand_Default_ReturnsICommand()
+        {
+            Assert.IsAssignableFrom<ICommand>(_viewModel.SearchCommand);
+        }
+
+        [Fact]
+        public void SearchQuery_Default_ReturnsEmptyString()
+        {
+            Assert.Equal(string.Empty, _viewModel.SearchQuery);
+        }
+
+        [Fact]
+        public void HasResults_Default_ReturnsFalse()
+        {
+            Assert.False(_viewModel.HasResults);
+        }
+
+        [Fact]
+        public void IsBusy_Default_ReturnsFalse()
+        {
+            Assert.False(_viewModel.IsBusy);
+        }
+    }
+
+    internal class TestPaginationSearchViewModelBaseTests : PaginationSearchViewModelBase<string, string>
+    {
+        protected override IList<string> MapItemsToViewModels(IList<string> models)
+        {
+            return models;
+        }
+
+        protected override Task<PagingModel<string>> LoadAsync(string query, int pageNumber, int pageSize)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
