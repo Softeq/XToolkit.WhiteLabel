56780945ba6af96f4ab46985b152545cc9ce651e
Savikpavel <savikpavel@gmail.com>
2020-11-12 16:55:57 +0300

    LaunchedFromPushNotificationKey added to INotificationsSettingsParser; (#416)
    Documentation fixes
    
    Co-authored-by: savikpavel <pavel.savik@softeq.com>

--- Softeq.XToolkit.PushNotifications.Droid/NotificationIntentActivityInfo.cs
+++ Softeq.XToolkit.PushNotifications.Droid/NotificationIntentActivityInfo.cs
@@ -6,26 +6,32 @@ using System;
 namespace Softeq.XToolkit.PushNotifications.Droid
 {
     /// <summary>
-    /// Information about which Activity and how should be opened on tap on Notification
+    ///     Information about which Activity and how should be opened on tap on Notification.
     /// </summary>
     public class NotificationIntentActivityInfo
     {
         /// <summary>
-        /// Type of the Activity that shall be opened
+        ///     Initializes a new instance of the <see cref="NotificationIntentActivityInfo"/> class.
         /// </summary>
-        public Type ActivityType { get; set; }
-
-        /// <summary>
-        /// The value indicating whether parent stack should be created for this Activity
-        /// Note that to create parent stack or an activity in separate task you need to add some directives in manifest/activity declaration.
-        /// For more details <see href="https://developer.android.com/training/notify-user/navigation"/>
-        /// </summary>
-        public bool DoCreateParentStack { get; set; }
-
+        /// <param name="activityType">The type of the Activity that shall be opened.</param>
+        /// <param name="doCreateParentStack">Value indicating whether parent stack should be created for this Activity.</param>
         public NotificationIntentActivityInfo(Type activityType, bool doCreateParentStack)
         {
             ActivityType = activityType;
             DoCreateParentStack = doCreateParentStack;
         }
+
+        /// <summary>
+        ///     Gets or sets the type of the Activity that shall be opened.
+        /// </summary>
+        public Type ActivityType { get; set; }
+
+        /// <summary>
+        ///     Gets or sets a value indicating whether parent stack should be created for this Activity.
+        ///     <para/>
+        ///     Note that to create parent stack or an activity in separate task you need to add some directives in manifest/activity declaration.
+        ///     For more details <see href="https://developer.android.com/training/notify-user/navigation"/>.
+        /// </summary>
+        public bool DoCreateParentStack { get; set; }
     }
 }
