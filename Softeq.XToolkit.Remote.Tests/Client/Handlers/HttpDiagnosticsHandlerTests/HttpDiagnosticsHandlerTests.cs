4b07319d33acc1cfeb9cf70c8e23b10da7a7e9de
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-12-07 20:48:34 +0300

    Rework diagnostic handler & add log verbosity

---
+++ Softeq.XToolkit.Remote.Tests/Client/Handlers/HttpDiagnosticsHandlerTests/HttpDiagnosticsHandlerTests.cs
@@ -0,0 +1,148 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using NSubstitute;
+using Softeq.XToolkit.Common.Logger;
+using Softeq.XToolkit.Remote.Client.Handlers;
+using Softeq.XToolkit.Remote.Primitives;
+using Softeq.XToolkit.Remote.Tests.Auth.Handlers.RefreshTokenHttpClientHandlerTests;
+using Xunit;
+
+namespace Softeq.XToolkit.Remote.Tests.Client.Handlers.HttpDiagnosticsHandlerTests
+{
+    public class HttpDiagnosticsHandlerTests
+    {
+        private readonly HttpRequestMessage _request;
+        private readonly ILogger _logger;
+        private readonly object? _mockInternalSendAsync;
+        private readonly HttpMessageHandler _innerHandler;
+
+        public HttpDiagnosticsHandlerTests()
+        {
+            _request = new HttpRequestMessage
+            {
+                Content = new StringContent("test data")
+            };
+
+            _logger = Substitute.For<ILogger>();
+
+            _innerHandler = Substitute.For<HttpMessageHandler>();
+            _mockInternalSendAsync = _innerHandler.Protected("SendAsync", Arg.Any<HttpRequestMessage>(), Arg.Any<CancellationToken>());
+            _mockInternalSendAsync.Returns(Task.FromResult(new HttpResponseMessage()));
+        }
+
+        private string RequestTitle => Arg.Is<string>(x => x.Contains("Request:"));
+        private string RequestBodyTitle => Arg.Is<string>(x => x.Contains("Request Content"));
+        private string ResponseTitle => Arg.Is<string>(x => x.Contains("Response:"));
+        private string ResponseBodyTitle => Arg.Is<string>(x => x.Contains("Response Content"));
+
+        [Fact]
+        public void Ctor_Null_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                new HttpDiagnosticsHandler(null!, LogVerbosity.Unspecified);
+            });
+        }
+
+        [Fact]
+        public async Task SendAsync_WithDefaultVerbosity_ExecutesForAll()
+        {
+            var handler = CreateHandlerWithVerbosity(LogVerbosity.Unspecified);
+
+            await handler.PublicSendAsync(_request, CancellationToken.None);
+
+            _logger.Received(1).Debug(RequestTitle);
+            _logger.Received(1).Debug(RequestBodyTitle);
+            _logger.Received(1).Debug(ResponseTitle);
+            _logger.Received(1).Debug(ResponseBodyTitle);
+        }
+
+        [Fact]
+        public async Task SendAsync_WithNoneVerbosity_ExecutesForAll()
+        {
+            var handler = CreateHandlerWithVerbosity(LogVerbosity.None);
+
+            await handler.PublicSendAsync(_request, CancellationToken.None);
+
+            _logger.DidNotReceive().Debug(RequestTitle);
+            _logger.DidNotReceive().Debug(RequestBodyTitle);
+            _logger.DidNotReceive().Debug(ResponseTitle);
+            _logger.DidNotReceive().Debug(ResponseBodyTitle);
+        }
+
+        [Fact]
+        public async Task SendAsync_WithRequestAllVerbosity_ExecutesForAll()
+        {
+            var handler = CreateHandlerWithVerbosity(LogVerbosity.RequestAll);
+
+            await handler.PublicSendAsync(_request, CancellationToken.None);
+
+            _logger.Received(1).Debug(RequestTitle);
+            _logger.Received(1).Debug(RequestBodyTitle);
+        }
+
+        [Fact]
+        public async Task SendAsync_WithRequestHeadersVerbosity_ExecutesForAll()
+        {
+            var handler = CreateHandlerWithVerbosity(LogVerbosity.RequestHeaders);
+
+            await handler.PublicSendAsync(_request, CancellationToken.None);
+
+            _logger.Received(1).Debug(RequestTitle);
+        }
+
+        [Fact]
+        public async Task SendAsync_WithRequestBodyVerbosity_ExecutesForAll()
+        {
+            var handler = CreateHandlerWithVerbosity(LogVerbosity.RequestBody);
+
+            await handler.PublicSendAsync(_request, CancellationToken.None);
+
+            _logger.Received(1).Debug(RequestBodyTitle);
+        }
+
+        [Fact]
+        public async Task SendAsync_WithResponseAllVerbosity_ExecutesForAll()
+        {
+            var handler = CreateHandlerWithVerbosity(LogVerbosity.ResponseAll);
+
+            await handler.PublicSendAsync(_request, CancellationToken.None);
+
+            _logger.Received(1).Debug(ResponseTitle);
+            _logger.Received(1).Debug(ResponseBodyTitle);
+        }
+
+        [Fact]
+        public async Task SendAsync_WithResponseHeadersVerbosity_ExecutesForAll()
+        {
+            var handler = CreateHandlerWithVerbosity(LogVerbosity.ResponseHeaders);
+
+            await handler.PublicSendAsync(_request, CancellationToken.None);
+
+            _logger.Received(1).Debug(ResponseTitle);
+        }
+
+        [Fact]
+        public async Task SendAsync_WithResponseBodyVerbosity_ExecutesForAll()
+        {
+            var handler = CreateHandlerWithVerbosity(LogVerbosity.ResponseBody);
+
+            await handler.PublicSendAsync(_request, CancellationToken.None);
+
+            _logger.Received(1).Debug(ResponseBodyTitle);
+        }
+
+        private TestHttpDiagnosticsHandler CreateHandlerWithVerbosity(LogVerbosity verbosity)
+        {
+            return new TestHttpDiagnosticsHandler(_logger, verbosity)
+            {
+                InnerHandler = _innerHandler
+            };
+        }
+    }
+}
--- Softeq.XToolkit.Remote/Client/Handlers/HttpDiagnosticsHandler.cs
+++ Softeq.XToolkit.Remote/Client/Handlers/HttpDiagnosticsHandler.cs
@@ -4,61 +4,96 @@
 using System;
 using System.Diagnostics;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
 using Softeq.XToolkit.Common.Logger;
+using Softeq.XToolkit.Remote.Primitives;
 
 namespace Softeq.XToolkit.Remote.Client.Handlers
 {
-    internal class HttpDiagnosticsHandler : DelegatingHandler
+    public class HttpDiagnosticsHandler : DelegatingHandler
     {
         private readonly ILogger _logger;
+        private readonly LogVerbosity _verbosity;
 
-        public HttpDiagnosticsHandler(ILogger logger)
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="HttpDiagnosticsHandler"/> class.
+        /// </summary>
+        /// <param name="logger">Instance of <see cref="ILogger"/>.</param>
+        /// <param name="verbosity">
+        ///     Instance of verbosity bitmask, setting the instance verbosity overrides <see cref="DefaultVerbosity"/>.
+        /// </param>
+        public HttpDiagnosticsHandler(ILogger logger, LogVerbosity verbosity)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _verbosity = verbosity == LogVerbosity.Unspecified ? DefaultVerbosity : verbosity;
         }
 
+        /// <summary>
+        ///     Gets or sets default verbosity bitmask <see cref="LogVerbosity"/>.
+        /// </summary>
+        public static LogVerbosity DefaultVerbosity { get; set; } = LogVerbosity.All;
+
+        /// <inheritdoc />
         protected override async Task<HttpResponseMessage> SendAsync(
             HttpRequestMessage request,
             CancellationToken cancellationToken)
         {
             var totalElapsedTime = Stopwatch.StartNew();
 
-            WriteMessage($"Request: {request}");
-            if (request.Content != null)
-            {
-                var content = await request.Content.ReadAsStringAsync().ConfigureAwait(false);
-                WriteMessage($"Request Content: {content}");
-            }
+            await LogHttpRequestAsync(request).ConfigureAwait(false);
 
             var responseElapsedTime = Stopwatch.StartNew();
+
             var response = await base.SendAsync(request, cancellationToken);
 
-            WriteMessage($"Response: {response}");
-
-            // Prevent read all content when response chunked or empty
-            if (response.Content?.Headers.ContentLength != null)
-            {
-                var content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-                WriteMessage($"Response Content: {content}");
-            }
-            else
-            {
-                WriteMessage("Response Content or Content-Length: null");
-            }
+            await LogHttpResponseAsync(response);
 
             responseElapsedTime.Stop();
-            WriteMessage($"Response elapsed time: {responseElapsedTime.ElapsedMilliseconds} ms");
+            Log($"Response elapsed time: {responseElapsedTime.ElapsedMilliseconds} ms");
 
             totalElapsedTime.Stop();
-            WriteMessage($"Total elapsed time: {totalElapsedTime.ElapsedMilliseconds} ms");
+            Log($"Total elapsed time: {totalElapsedTime.ElapsedMilliseconds} ms");
 
             return response;
         }
 
-        private void WriteMessage(string message)
+        protected virtual async Task LogHttpRequestAsync(HttpRequestMessage request)
+        {
+            if (_verbosity.HasFlag(LogVerbosity.RequestHeaders))
+            {
+                Log($"Request: {request}");
+            }
+
+            if (_verbosity.HasFlag(LogVerbosity.RequestBody) && request.Content != null)
+            {
+                var content = await request.Content.ReadAsStringAsync().ConfigureAwait(false);
+                Log($"Request Content: {content}");
+            }
+        }
+
+        protected virtual async Task LogHttpResponseAsync(HttpResponseMessage? response)
+        {
+            if (_verbosity.HasFlag(LogVerbosity.ResponseHeaders))
+            {
+                Log($"Response: {response}");
+            }
+
+            if (_verbosity.HasFlag(LogVerbosity.ResponseBody) && response != null)
+            {
+                if (response.Content?.Headers.ContentLength == null)
+                {
+                    Log("Response Content or Content-Length: null");
+                }
+                else
+                {
+                    var content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                    Log($"Response Content: {content}");
+                }
+            }
+        }
+
+        private void Log(string message)
         {
             _logger.Debug(message);
         }
