80d6105ab99c6a13193412b0294fdcfc0ee3b72f
Sergey Levshunov <sergeylevshunov@Sergeys-MacBook-Pro.local>
2021-09-14 14:51:23 +0200

    fix pr

--- Softeq.XToolkit.WhiteLabel.Droid/Providers/ContextProvider.cs
+++ Softeq.XToolkit.WhiteLabel.Droid/Providers/ContextProvider.cs
@@ -4,45 +4,72 @@
 using System;
 using Android.App;
 using Android.Content;
-using Softeq.XToolkit.Common.Weak;
+using Xamarin.Essentials;
 
 #nullable disable
 
 namespace Softeq.XToolkit.WhiteLabel.Droid.Providers
 {
+    public enum ActivityState
+    {
+        Created,
+        Resumed,
+        Paused,
+        Destroyed,
+        SaveInstanceState,
+        Started,
+        Stopped
+    }
+
     public interface IContextProvider
     {
-        event EventHandler CurrentActivityChanged;
+        event EventHandler<ActivityStateChangedEventArgs> ActivityStateChanged;
 
         Activity CurrentActivity { get; }
 
         Context AppContext { get; }
     }
 
-    public class EssentialsContextProvider : IContextProvider
+    public class ActivityStateChangedEventArgs : EventArgs
     {
-        private WeakReferenceEx<Activity> _activityRef;
-
-        public EssentialsContextProvider()
+        internal ActivityStateChangedEventArgs(Activity activity, ActivityState state)
         {
-            _activityRef = WeakReferenceEx.Create(CurrentActivity);
-            Xamarin.Essentials.Platform.ActivityStateChanged += OnPlatformActivityStateChanged;
+            State = state;
+            Activity = activity;
         }
 
-        public event EventHandler CurrentActivityChanged;
+        public ActivityState State { get; }
 
-        public Activity CurrentActivity => Xamarin.Essentials.Platform.CurrentActivity;
+        public Activity Activity { get; }
+    }
 
-        public Context AppContext => Xamarin.Essentials.Platform.AppContext;
-
-        private void OnPlatformActivityStateChanged(object sender, Xamarin.Essentials.ActivityStateChangedEventArgs e)
+    public class EssentialsContextProvider : IContextProvider
+    {
+        public EssentialsContextProvider()
         {
-            if (_activityRef.Target != CurrentActivity)
-            {
-                CurrentActivityChanged?.Invoke(this, EventArgs.Empty);
-            }
+            Platform.ActivityStateChanged += (sender, args) =>
+                ActivityStateChanged?.Invoke(sender, new ActivityStateChangedEventArgs(args.Activity, ToLifecycleState( args.State)));
+        }
 
-            _activityRef = WeakReferenceEx.Create(CurrentActivity);
+        public event EventHandler<ActivityStateChangedEventArgs> ActivityStateChanged;
+
+        public Activity CurrentActivity => Platform.CurrentActivity;
+
+        public Context AppContext => Platform.AppContext;
+
+        private static ActivityState ToLifecycleState(Xamarin.Essentials.ActivityState activityState)
+        {
+            return activityState switch
+            {
+                Xamarin.Essentials.ActivityState.Created => ActivityState.Created,
+                Xamarin.Essentials.ActivityState.Destroyed => ActivityState.Destroyed,
+                Xamarin.Essentials.ActivityState.Paused => ActivityState.Paused,
+                Xamarin.Essentials.ActivityState.Resumed => ActivityState.Resumed,
+                Xamarin.Essentials.ActivityState.SaveInstanceState => ActivityState.SaveInstanceState,
+                Xamarin.Essentials.ActivityState.Started => ActivityState.Started,
+                Xamarin.Essentials.ActivityState.Stopped => ActivityState.Stopped,
+                _ => throw new NotImplementedException()
+            };
         }
     }
 }
