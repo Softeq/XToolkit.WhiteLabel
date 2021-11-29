5934f584e73243c3a60eb1649d366b56b63870be
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2021-11-29 14:46:12 +0300

    Add basic feature contracts

---
+++ Softeq.XToolkit.Common/Files/IStorageInfoProvider.cs
@@ -0,0 +1,19 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Softeq.XToolkit.Common.Files
+{
+    /// <summary>
+    ///     Provides info about available storages.
+    /// </summary>
+    public interface IStorageInfoProvider
+    {
+        /// <summary>
+        ///     Gets info about available storages.
+        /// </summary>
+        List<StorageInfo> Current { get; }
+    }
+}
---
+++ Softeq.XToolkit.Common/Files/StorageInfo.cs
@@ -0,0 +1,57 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+using System;
+
+namespace Softeq.XToolkit.Common.Files
+{
+    public readonly struct StorageInfo : IEquatable<StorageInfo>
+    {
+        public StorageInfo(StorageType type, ulong totalBytes, ulong freeBytes)
+        {
+            Type = type;
+            TotalBytes = totalBytes;
+            FreeBytes = freeBytes;
+        }
+
+        public StorageType Type { get; }
+
+        public ulong TotalBytes { get; }
+
+        public ulong FreeBytes { get; }
+
+        /// <inheritdoc />
+        public bool Equals(StorageInfo other) =>
+            Type == other.Type &&
+            FreeBytes == other.FreeBytes &&
+            TotalBytes == other.TotalBytes;
+
+        /// <inheritdoc />
+        public static bool operator ==(StorageInfo lhs, StorageInfo rhs) => lhs.Equals(rhs);
+
+        /// <inheritdoc />
+        public static bool operator !=(StorageInfo lhs, StorageInfo rhs) => !(lhs == rhs);
+
+        /// <inheritdoc />
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            return obj is StorageInfo info && Equals(info);
+        }
+
+        public override int GetHashCode() => (TotalBytes, FreeBytes, Type).GetHashCode();
+
+        public override string ToString() => $"Type: {Type}, Total: {TotalBytes}, Free: {FreeBytes}";
+    }
+
+    public enum StorageType
+    {
+        Unknown = 0,
+        Internal = 1,
+        External = 2
+    }
+}
