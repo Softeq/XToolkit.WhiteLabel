e8cd5090c0a0228554324d7f0a02aae47839bc11
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2022-01-19 23:58:08 +0300

    Change build script

--- documentation/build.sh
+++ documentation/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-msbuild -r Softeq.XToolkit.Documentation.csproj
+# msbuild -r Softeq.XToolkit.Documentation.csproj
 
-# mono ~/.nuget/packages/docfx.console/2.43.2/tools/docfx.exe docfx.json
+DOCFX_VERSION="2.58.9"
+DOCFX_PATH="$HOME/.nuget/packages/docfx.console/$DOCFX_VERSION/tools/docfx.exe"
+DOCFX_CONFIG_PATH="docfx.json"
+
+mono $DOCFX_PATH metadata $DOCFX_CONFIG_PATH
+sudo pwsh unflatten-namespaces.ps1 ./obj/docfxapi/toc.yml
+mono $DOCFX_PATH build $DOCFX_CONFIG_PATH
