c8e042b9cca56a99327494ef1d6555168c5389c4
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2022-01-24 22:52:57 +0300

    Rework platfrom launch services to use essentials impl

--- Softeq.XToolkit.WhiteLabel.iOS/Services/IosLauncherService.cs
+++ Softeq.XToolkit.WhiteLabel.iOS/Services/IosLauncherService.cs
@@ -1,64 +1,35 @@
 ﻿// Developed by Softeq Development Corporation
 // http://www.softeq.com
 
-using System;
 using AVFoundation;
 using AVKit;
 using Foundation;
-using Softeq.XToolkit.WhiteLabel.Interfaces;
+using Softeq.XToolkit.Common.Logger;
 using Softeq.XToolkit.WhiteLabel.iOS.Navigation;
-using UIKit;
+using Softeq.XToolkit.WhiteLabel.Services;
 
 namespace Softeq.XToolkit.WhiteLabel.iOS.Services
 {
-    public class IosLauncherService : ILauncherService
+    public class IosLauncherService : EssentialsLauncherService
     {
         private readonly IViewLocator _viewLocator;
 
-        public IosLauncherService(IViewLocator viewLocator)
+        public IosLauncherService(
+            IViewLocator viewLocator,
+            ILogManager logManager)
+            : base(logManager)
         {
             _viewLocator = viewLocator;
         }
 
-        public void OpenUrl(string urlStr)
+        public override void OpenVideo(string videoUrl)
         {
-            var uri = new Uri(urlStr);
-            var url = new NSUrl(uri.AbsoluteUri);
-            var app = UIApplication.SharedApplication;
+            var topViewController = _viewLocator.GetTopViewController();
+            var playerViewController = new AVPlayerViewController();
 
-            if (app.CanOpenUrl(url))
-            {
-                app.OpenUrl(url);
-            }
-        }
-
-        public void OpenAppSettings()
-        {
-            UIApplication.SharedApplication.OpenUrl(new NSUrl(UIApplication.OpenSettingsUrlString));
-        }
-
-        public void OpenVideo(string videoUrl)
-        {
-            var vc = _viewLocator.GetTopViewController();
-            var controller = new AVPlayerViewController();
-
-            if (controller.Player == null)
-            {
-                controller.Player = new AVPlayer(NSUrl.FromString(videoUrl));
-            }
-
-            vc.PresentViewController(controller, true, null);
-            controller.Player.Play();
-        }
-
-        public void OpenEmail(string email)
-        {
-            OpenUrl($"mailto:{email}");
-        }
-
-        public void OpenPhoneNumber(string number)
-        {
-            OpenUrl($"tel:{number}");
+            playerViewController.Player ??= new AVPlayer(NSUrl.FromString(videoUrl));
+            topViewController.PresentViewController(playerViewController, true, null);
+            playerViewController.Player.Play();
         }
     }
 }
