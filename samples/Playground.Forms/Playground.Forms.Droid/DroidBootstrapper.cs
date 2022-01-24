28f17c84ce18e598c3796e3c4a8d35144807d5be
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2022-01-24 22:53:45 +0300

    Add launcher sample to playground.forms

--- samples/Playground.Forms/Playground.Forms.Droid/DroidBootstrapper.cs
+++ samples/Playground.Forms/Playground.Forms.Droid/DroidBootstrapper.cs
@@ -10,6 +10,9 @@ using Softeq.XToolkit.Common.Extensions;
 using Softeq.XToolkit.Permissions;
 using Softeq.XToolkit.Permissions.Droid;
 using Softeq.XToolkit.WhiteLabel.Bootstrapper.Abstract;
+using Softeq.XToolkit.WhiteLabel.Droid.Providers;
+using Softeq.XToolkit.WhiteLabel.Droid.Services;
+using Softeq.XToolkit.WhiteLabel.Interfaces;
 
 namespace Playground.Forms.Droid
 {
@@ -29,6 +32,10 @@ namespace Playground.Forms.Droid
             builder.Singleton<PermissionsService, IPermissionsService>();
             builder.Singleton<PermissionsManager, IPermissionsManager>();
             builder.Singleton<RequestResultHandler, IPermissionRequestHandler>();
+
+            // launcher
+            builder.Singleton<EssentialsContextProvider, IContextProvider>();
+            builder.Singleton<DroidLauncherService, ILauncherService>();
 
             // remote
             // - example of using custom primary http message handler
---
+++ samples/Playground.Forms/Playground.Forms/ViewModels/Components/LauncherPageViewModel.cs
@@ -0,0 +1,36 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+using System.Windows.Input;
+using Softeq.XToolkit.Common.Commands;
+using Softeq.XToolkit.WhiteLabel.Interfaces;
+using Softeq.XToolkit.WhiteLabel.Mvvm;
+
+namespace Playground.Forms.ViewModels.Components
+{
+    public class LauncherPageViewModel : ViewModelBase
+    {
+        private readonly ILauncherService _launcherService;
+
+        public LauncherPageViewModel(ILauncherService launcherService)
+        {
+            _launcherService = launcherService;
+
+            OpenUrlCommand = new RelayCommand(() => _launcherService.OpenUrl("https://softeq.com"));
+            OpenAppSettingsCommand = new RelayCommand(() => _launcherService.OpenAppSettings());
+            OpenVideoCommand = new RelayCommand(() => _launcherService.OpenVideo("https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"));
+            OpenEmailCommand = new RelayCommand(() => _launcherService.OpenEmail("test@example.com"));
+            OpenPhoneNumberCommand = new RelayCommand(() => _launcherService.OpenPhoneNumber("1234455"));
+        }
+
+        public ICommand OpenUrlCommand { get; }
+
+        public ICommand OpenAppSettingsCommand { get; }
+
+        public ICommand OpenVideoCommand { get; }
+
+        public ICommand OpenEmailCommand { get; }
+
+        public ICommand OpenPhoneNumberCommand { get; }
+    }
+}
