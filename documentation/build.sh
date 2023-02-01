c49ed8ebf100c2328b82ed1fb881b77719285f72
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2023-02-01 12:41:55 +0200

    WIP: Use nested namespaces from DocFX

--- documentation/build.sh
+++ documentation/build.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-# Build via csproj
-# msbuild -r Softeq.XToolkit.Documentation.csproj
-
-DOCFX_VERSION="2.60.2"
+DOCFX_VERSION="2.62.0" # see https://github.com/dotnet/docfx/pull/8371
 DOCFX_CONFIG_PATH="docfx.json"
 
 dotnet tool install -g docfx --version $DOCFX_VERSION
-
-# Prepate toc.yml
-docfx metadata $DOCFX_CONFIG_PATH
-
-# Apply patch to toc.yml
-sudo pwsh nested-namespaces.ps1 ./obj/docfxapi/toc.yml
 
 # Build documentation
 docfx build $DOCFX_CONFIG_PATH
