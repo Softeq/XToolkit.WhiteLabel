307c7eb62d27cc23b825b46de42176196eba10db
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2023-02-21 15:26:44 +0200

    Update build script

--- documentation/build.sh
+++ documentation/build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-DOCFX_VERSION="2.62.0" # see https://github.com/dotnet/docfx/pull/8371
+DOCFX_VERSION="2.62.1" # see https://github.com/dotnet/docfx/pull/8371
 DOCFX_CONFIG_PATH="docfx.json"
 
-dotnet tool install -g docfx --version $DOCFX_VERSION
+# install tool
+dotnet tool update -g docfx --version $DOCFX_VERSION
 
-# Build documentation
+# build documentation
 docfx build $DOCFX_CONFIG_PATH
+
+# dev server
+#docfx $DOCFX_CONFIG_PATH --serve
