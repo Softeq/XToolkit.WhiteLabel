8ba3d917d57bad96ecdb3876a9f0ccac65c92245
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2023-02-28 23:06:00 +0200

    Add newtosoftjson tests

---
+++ Softeq.XToolkit.WhiteLabel.Tests/Services/NewtonsoftJsonSerializerTests/NewtonsoftJsonSerializerTests.cs
@@ -0,0 +1,43 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+using Newtonsoft.Json;
+using Softeq.XToolkit.Common.Interfaces;
+using Softeq.XToolkit.WhiteLabel.Services;
+using Xunit;
+
+namespace Softeq.XToolkit.WhiteLabel.Tests.Services.NewtonsoftJsonSerializerTests
+{
+    public class NewtonsoftJsonSerializerTests
+    {
+        private readonly NewtonsoftJsonSerializer _serializer;
+
+        public NewtonsoftJsonSerializerTests()
+        {
+            _serializer = new NewtonsoftJsonSerializer();
+        }
+
+        [Fact]
+        public void DefaultSettings_StaticGet_ReturnsJsonSerializerSettings()
+        {
+            Assert.IsType<JsonSerializerSettings>(NewtonsoftJsonSerializer.DefaultSettings);
+        }
+
+        [Fact]
+        public void Ctor_Default_ReturnsIJsonSerializer()
+        {
+            Assert.IsAssignableFrom<IJsonSerializer>(new NewtonsoftJsonSerializer());
+        }
+
+        [Theory]
+        [MemberData(
+            nameof(NewtonsoftJsonSerializerTestsSerializeDataProvider.Data),
+            MemberType = typeof(NewtonsoftJsonSerializerTestsSerializeDataProvider))]
+        public void Serialize_CorrectData_ExpectedResult(StubSerializerData data, string expected)
+        {
+            var result = _serializer.Serialize(data);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
