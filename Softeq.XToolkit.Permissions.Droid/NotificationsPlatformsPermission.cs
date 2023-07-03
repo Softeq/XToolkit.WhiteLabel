28c5a0dde215b64e82eb89e34c677b74d4289963
Pavel Leonenko <pavel.leonenko@softeq.com>
2023-07-03 18:24:16 +0200

    Added handling Notifications permission on Android 13

---
+++ Softeq.XToolkit.Permissions.Droid/NotificationsPlatformsPermission.cs
@@ -0,0 +1,25 @@
+﻿// Developed for PAWS-HALO by Softeq Development Corporation
+// http://www.softeq.com
+
+using Android;
+using Android.OS;
+using BasePlatformPermission = Xamarin.Essentials.Permissions.BasePlatformPermission;
+
+namespace Softeq.XToolkit.Permissions.Droid
+{
+    public class NotificationsPlatformsPermission : BasePlatformPermission
+    {
+        public override (string androidPermission, bool isRuntime)[] RequiredPermissions
+        {
+            get
+            {
+                if (Build.VERSION.SdkInt >= BuildVersionCodes.Tiramisu)
+                {
+                    return new[] { (Manifest.Permission.PostNotifications, true) };
+                }
+
+                return new (string, bool)[] { };
+            }
+        }
+    }
+}
