a952656b2cb7d3cb9a884e56613e421c5b04f103
Pavel Leonenko <pavel.leonenko@softeq.com>
2023-07-04 14:24:46 +0200

    Adjusted NotificationsPlatformPermission code for lower Android versions

--- Softeq.XToolkit.Permissions.Droid/NotificationsPlatformPermission.cs
+++ Softeq.XToolkit.Permissions.Droid/NotificationsPlatformPermission.cs
@@ -1,8 +1,11 @@
 ﻿// Developed by Softeq Development Corporation
 // http://www.softeq.com
 
+#if __ANDROID_33__
 using Android;
 using Android.OS;
+using Xamarin.Essentials;
+#endif
 using BasePlatformPermission = Xamarin.Essentials.Permissions.BasePlatformPermission;
 
 namespace Softeq.XToolkit.Permissions.Droid
@@ -13,10 +16,13 @@ namespace Softeq.XToolkit.Permissions.Droid
         {
             get
             {
-                if (Build.VERSION.SdkInt >= BuildVersionCodes.Tiramisu)
+                #if __ANDROID_33__
+                if (Build.VERSION.SdkInt >= BuildVersionCodes.Tiramisu
+                    && Platform.AppContext.ApplicationInfo?.TargetSdkVersion >= BuildVersionCodes.Tiramisu)
                 {
                     return new[] { (Manifest.Permission.PostNotifications, true) };
                 }
+                #endif
 
                 return new (string, bool)[] { };
             }
