da8d58b463d27b567930cedc7d542d7152d024a3
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2023-08-02 23:30:09 +0200

    Migrate Common.iOS & Common.Droid Tests to .NET6 (#525)

---
+++ Softeq.XToolkit.Common.Droid.Tests/Platforms/Android/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Microsoft.Maui;
+
+namespace Softeq.XToolkit.Common.Droid.Tests;
+
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true,
+    ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode |
+                           ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+public class MainActivity : MauiAppCompatActivity
+{
+    public static MainActivity Current { get; private set; } = null!;
+
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        Current = this;
+
+        base.OnCreate(savedInstanceState);
+    }
+}
