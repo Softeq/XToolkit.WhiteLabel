9dddbfb8a866a5c9bd316674a93ff0b95dcab124
s.leushunou <siarhei.leushunou@softeq.com>
2024-10-03 13:34:28 +0200

    fix crash

--- Softeq.XToolkit.Common.iOS/Extensions/NSStringExtensions.cs
+++ Softeq.XToolkit.Common.iOS/Extensions/NSStringExtensions.cs
@@ -70,10 +70,17 @@ namespace Softeq.XToolkit.Common.iOS.Extensions
 
                 if (UIDevice.CurrentDevice.CheckSystemVersion(15, 0))
                 {
-                    var markdown = new Html2Markdown.Converter().Convert(html);
+                    if (string.IsNullOrEmpty(html))
+                    {
+                        attributedString = new NSAttributedString();
+                    }
+                    else
+                    {
+                        var markdown = new Html2Markdown.Converter().Convert(html);
 #pragma warning disable CS8601 // Possible null reference assignment.
-                    attributedString = new NSAttributedString(markdown, default, default, out error);
+                        attributedString = new NSAttributedString(markdown, default, default, out error);
 #pragma warning restore CS8601 // Possible null reference assignment.
+                    }
                 }
                 else
                 {
